4491b08edf9cadd2e488a967e7009cb72e7a820a
Raghavendra K T <raghavendra.kt@linux.vnet.ibm.com>
2014-02-14 10:51:56 +1100

    mm/readahead.c: fix readahead fail for no local memory and limit readahead pages
    max_sane_readahead() returns zero on the cpu having no local memory node.
    Fix that by returning a sanitized number of pages viz., minimum of
    (requested pages, 4k)
    
    Result:
    fadvise experiment with FADV_WILLNEED on a x240 machine with 1GB testfile
    32GB* 4G RAM  numa machine ( 12 iterations) yielded
    
    Kernel     Avg      Stddev
    base      7.2963    1.10 %
    patched   7.2972    1.18 %
    
    Signed-off-by: Raghavendra K T <raghavendra.kt@linux.vnet.ibm.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Cc: Wu Fengguang <fengguang.wu@intel.com>
    Cc: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/readahead.c
+++ mm/readahead.c
@@ -233,14 +233,32 @@ int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 	return 0;
 }
 
+#define MAX_REMOTE_READAHEAD   4096UL
 /*
  * Given a desired number of PAGE_CACHE_SIZE readahead pages, return a
  * sensible upper limit.
  */
 unsigned long max_sane_readahead(unsigned long nr)
 {
-	return min(nr, (node_page_state(numa_node_id(), NR_INACTIVE_FILE)
-		+ node_page_state(numa_node_id(), NR_FREE_PAGES)) / 2);
+	unsigned long local_free_page;
+	int nid;
+
+	nid = numa_node_id();
+	if (node_present_pages(nid)) {
+		/*
+		 * We sanitize readahead size depending on free memory in
+		 * the local node.
+		 */
+		local_free_page = node_page_state(nid, NR_INACTIVE_FILE)
+				 + node_page_state(nid, NR_FREE_PAGES);
+		return min(nr, local_free_page / 2);
+	}
+	/*
+	 * Readahead onto remote memory is better than no readahead when local
+	 * numa node does not have memory. We limit the readahead to 4k
+	 * pages though to avoid trashing page cache.
+	 */
+	return min(nr, MAX_REMOTE_READAHEAD);
 }
 
 /*
