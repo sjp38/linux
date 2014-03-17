e79363f0bf2bbddbf77583e955d3cd21d70e6a77
Jan Kara <jack@suse.cz>
2014-03-17 11:23:13 +1100

    backing_dev-fix-hung-task-on-sync-fix
    update changelog, add comment, use bdi_wakeup_thread_delayed()
    
    Reviewed-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Derek Basehore <dbasehore@chromium.org>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/fs-writeback.c
+++ fs/fs-writeback.c
@@ -1042,8 +1042,7 @@ void bdi_writeback_workfn(struct work_struct *work)
 	if (!list_empty(&bdi->work_list))
 		mod_delayed_work(bdi_wq, &wb->dwork, 0);
 	else if (wb_has_dirty_io(wb) && dirty_writeback_interval)
-		queue_delayed_work(bdi_wq, &wb->dwork,
-			msecs_to_jiffies(dirty_writeback_interval * 10));
+		bdi_wakeup_thread_delayed(bdi);
 
 	current->flags &= ~PF_SWAPWRITE;
 }
