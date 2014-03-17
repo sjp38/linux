8ae9d43a86cd06882e2af5453ef3d7860b08ce82
Andrew Morton <akpm@linux-foundation.org>
2014-03-17 11:23:38 +1100

    pagewalk-update-page-table-walker-core-fix-end-address-calculation-in-walk_page_range-fix
    s/min_t/min/
    
    Cc: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Cc: Sasha Levin <sasha.levin@oracle.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/pagewalk.c
+++ mm/pagewalk.c
@@ -321,7 +321,7 @@ int walk_page_range(unsigned long start, unsigned long end,
 			next = vma->vm_start;
 		} else { /* inside the found vma */
 			walk->vma = vma;
-			next = min_t(unsigned long, end, vma->vm_end);
+			next = min(end, vma->vm_end);
 
 			err = walk_page_test(start, next, walk);
 			if (skip_lower_level_walking(walk))
