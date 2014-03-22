1ff895c0955cf6bde2d46f73ebcfea951fbb2864
Andrew Morton <akpm@linux-foundation.org>
2014-03-22 12:49:47 +1100

    slub-use-raw_cpu_inc-for-incrementing-statistics-fix
    add comment
    
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Fengguang Wu <fengguang.wu@intel.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/slub.c
+++ mm/slub.c
@@ -224,6 +224,10 @@ static inline void memcg_propagate_slab_attrs(struct kmem_cache *s) { }
 static inline void stat(const struct kmem_cache *s, enum stat_item si)
 {
 #ifdef CONFIG_SLUB_STATS
+	/*
+	 * The rmw is racy on a preemptible kernel but this is acceptable, so
+	 * avoid this_cpu_add()'s irq-disable overhead.
+	 */
 	raw_cpu_inc(s->cpu_slab->stat[si]);
 #endif
 }
