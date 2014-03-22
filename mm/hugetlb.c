0f87df656d4c1d34a83eadb0511177c1480adee3
Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
2014-03-22 12:32:25 +1100

    mm-hugetlbfs-fix-rmapping-for-anonymous-hugepages-with-page_pgoff-v2
    - fix wrong shift direction
    - introduce page_size_order() and huge_page_size_order()
    - move the declaration of PageHuge() to include/linux/hugetlb_inline.h
      to avoid macro definition.
    
    Signed-off-by: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Cc: Sasha Levin <sasha.levin@oracle.com>
    Cc: Rik van Riel <riel@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -30,6 +30,7 @@
 
 #include <linux/io.h>
 #include <linux/hugetlb.h>
+#include <linux/hugetlb_inline.h>
 #include <linux/hugetlb_cgroup.h>
 #include <linux/node.h>
 #include "internal.h"
@@ -764,9 +765,9 @@ pgoff_t __basepage_index(struct page *page)
 	return (index << compound_order(page_head)) + compound_idx;
 }
 
-pgoff_t hugepage_pgoff(struct page *page)
+unsigned int huge_page_size_order(struct page *page)
 {
-	return page->index << huge_page_order(page_hstate(page));
+	return huge_page_order(page_hstate(page));
 }
 
 static struct page *alloc_fresh_huge_page_node(struct hstate *h, int nid)
