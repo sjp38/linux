a0395882610ae31d3ca3da6279f14b62eccbfea2
SeongJae Park <sj38.park@gmail.com>
2014-05-13 21:24:12 +0900

    GCMA: evaluation: evaluate using debugfs
    Before this commit, evaluation of GCMA was done using a driver by
    loading it to let evaluation available during specific situation like
    memory stress after boot completed. With this commit, polish evaluation
    code more(let user specify evaluation workload - number of requesting
    contig page size, warmup time, number of trials, warmup time) and let it
    be triggered using debugfs.
    
    User can do evaluation whenever he want by writing number of pages,
    number of trials, warmup time in miliseconds to debugfs's `eval` file.
    
    The result can be seen by kernel message. Whole results can be seen by
    reading debugfs's `res` file.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- mm/test_gcma.c
+++ mm/test_gcma.c
@@ -27,6 +27,11 @@
 #include <linux/gcma.h>
 #include <linux/mm.h>
 #include <linux/dma-contiguous.h>
+#include <linux/debugfs.h>
+#include <asm/uaccess.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+#include <linux/list.h>
 
 /*********************************
 * tunables
@@ -216,6 +221,214 @@ static int test_alloc_release_contig(void)
 	return 0;
 }
 
+
+/**************
+ * evaluation
+ **************/
+
+struct eval_option {
+	int nr_pages;
+	int nr_trials;
+	unsigned long warmup_ms;
+};
+
+struct eval_result {
+	unsigned long succ;
+	unsigned long fail;
+	unsigned long min;
+	unsigned long max;
+	unsigned long avg;
+	unsigned long stddev;
+};
+
+struct eval_history {
+	struct eval_option option;
+	struct eval_result cma_result;
+	struct eval_result gcma_result;
+	struct list_head node;
+};
+
+static struct kmem_cache *eval_history_cache;
+static LIST_HEAD(eval_history_list);
+
+/**
+ * howto_read - just show how to use evaluation feature
+ */
+static ssize_t howto_read(struct file *filp, char __user *buf,
+				size_t length, loff_t *offset)
+{
+	static char *howto_msg = "howto:\n"
+		"\t<# of pages> <# of trials> <warmup_ms> > eval\n";
+	char *cursor = howto_msg + *offset;
+	int bytes_read = 0;
+
+	while (length && *cursor) {
+		put_user(*(cursor++), buf++);
+
+		length--;
+		bytes_read++;
+	}
+
+	*offset += bytes_read;
+	return bytes_read;
+}
+
+static const struct file_operations howto_fops = {
+	.owner = THIS_MODULE,
+	.open = nonseekable_open,
+	.read = howto_read,
+	.llseek = no_llseek,
+};
+
+static ssize_t eval_write(struct file *filp, const char __user *buf,
+				size_t length, loff_t *offset)
+{
+	struct eval_history *history;
+	struct eval_option *opt;
+	struct eval_result *cma_res, *gcma_res;
+
+	unsigned long gcma_time, cma_time;
+	unsigned long gcma_sum = 0, cma_sum = 0;
+
+	int i;
+
+	history = kmem_cache_alloc(eval_history_cache, GFP_KERNEL);
+	if (history == NULL) {
+		pr_warn("failed to alloc eval history\n");
+		return length;
+	}
+
+	opt = &history->option;
+	cma_res = &history->cma_result;
+	gcma_res = &history->gcma_result;
+
+	cma_res->min = cma_res->max = cma_res->succ = cma_res->fail = 0;
+	gcma_res->min = gcma_res->max = gcma_res->succ = gcma_res->fail = 0;
+
+	sscanf(buf, "%d %d %ld", &opt->nr_pages, &opt->nr_trials,
+			&opt->warmup_ms);
+	pr_info("%s called with %d %d %ld\n", __func__, opt->nr_pages,
+			opt->nr_trials, opt->warmup_ms);
+
+	for (i = 0; i < opt->nr_trials; i++) {
+		cma_time = measure_cma(opt->nr_pages);
+		msleep(opt->warmup_ms);
+
+		gcma_time = measure_gcma(opt->nr_pages);
+		msleep(opt->warmup_ms);
+
+		if (!cma_time)
+			cma_res->fail++;
+		else
+			cma_res->succ++;
+		if (!gcma_time)
+			gcma_res->fail++;
+		else
+			gcma_res->succ++;
+
+		cma_res->min = min_of(cma_res->min, cma_time);
+		cma_res->max = max_of(cma_res->max, cma_time);
+		cma_sum += cma_time;
+
+		gcma_res->min = min_of(gcma_res->min, gcma_time);
+		gcma_res->max = max_of(gcma_res->max, gcma_time);
+		gcma_sum += gcma_time;
+	}
+	gcma_res->avg = gcma_sum / opt->nr_trials;
+	cma_res->avg = cma_sum / opt->nr_trials;
+
+	pr_info("gcma\t,%d,pages\t,succ/fail:(,%ld,%ld)\t,min: ,%ld\t,max: ,%ld\t,avg: ,%ld\n",
+			opt->nr_pages, gcma_res->succ, gcma_res->fail,
+			gcma_res->min, gcma_res->max, gcma_res->avg);
+	pr_info("cma\t,%d,pages\t,succ/fail:(,%ld,%ld)\t,min: ,%ld\t,max: ,%ld\t,avg: ,%ld\n",
+			opt->nr_pages, cma_res->succ, cma_res->fail,
+			cma_res->min, cma_res->max, cma_res->avg);
+
+	list_add_tail(&history->node, &eval_history_list);
+
+	return length;
+}
+
+static const struct file_operations eval_fops = {
+	.owner = THIS_MODULE,
+	.open = nonseekable_open,
+	.write = eval_write,
+	.llseek = no_llseek,
+};
+
+static ssize_t eval_res_read(struct file *filp, char __user *buf,
+				size_t length, loff_t *offset)
+{
+	struct eval_history *history;
+	struct eval_option *opt;
+	struct eval_result *gcma_res, *cma_res;
+	char kbuf[256];
+	char *cursor = kbuf;
+	int bytes_read = 0;
+
+	pr_info("%s called. length: %d, offset: %d\n",
+			__func__, (int)length, (int)*offset);
+	if (*offset > 0)
+		return 0;
+
+	list_for_each_entry(history, &eval_history_list, node) {
+		opt = &history->option;
+		gcma_res = &history->gcma_result;
+		cma_res = &history->cma_result;
+
+		pr_info("gcma\t,%d,pages\t,succ/fail:(,%ld,%ld)\t,min: ,%ld\t,max: ,%ld\t,avg: ,%ld\n",
+				opt->nr_pages, gcma_res->succ, gcma_res->fail,
+				gcma_res->min, gcma_res->max, gcma_res->avg);
+		pr_info("cma\t,%d,pages\t,succ/fail:(,%ld,%ld)\t,min: ,%ld\t,max: ,%ld\t,avg: ,%ld\n",
+				opt->nr_pages, cma_res->succ, cma_res->fail,
+				cma_res->min, cma_res->max, cma_res->avg);
+
+		sprintf(kbuf, "%d,%ld,%ld,%ld,%ld,%ld\n,%d,%ld,%ld,%ld,%ld,%ld\n",
+				opt->nr_pages, gcma_res->succ, gcma_res->fail,
+				gcma_res->min, gcma_res->max, gcma_res->avg,
+				opt->nr_pages, cma_res->succ, cma_res->fail,
+				cma_res->min, cma_res->max, cma_res->avg);
+
+		cursor = kbuf;
+		while (length && *cursor) {
+			put_user(*(cursor++), buf++);
+
+			length--;
+			bytes_read++;
+		}
+
+		*offset += bytes_read;
+
+	}
+
+
+	return bytes_read;
+}
+
+static const struct file_operations result_fops = {
+	.owner = THIS_MODULE,
+	.open = nonseekable_open,
+	.read = eval_res_read,
+	.llseek = no_llseek,
+};
+
+static struct dentry *debugfs_root;
+
+static int __init debugfs_init(void)
+{
+	debugfs_root = debugfs_create_dir("gcma_eval", NULL);
+	if (!debugfs_root) {
+		pr_err("failed to create debugfs\n");
+		return -ENOMEM;
+	}
+
+	debugfs_create_file("howto", S_IRUSR, debugfs_root, NULL, &howto_fops);
+	debugfs_create_file("eval", S_IWUSR, debugfs_root, NULL, &eval_fops);
+	debugfs_create_file("res", S_IRUSR, debugfs_root, NULL, &result_fops);
+	return 0;
+}
+
+
 #define do_test(test)					\
 	do {						\
 		if (test()) {				\
@@ -233,6 +446,14 @@ static int __init init_gcma(void)
 	if (!enabled)
 		return 0;
 	pr_info("test gcma\n");
+
+	eval_history_cache = KMEM_CACHE(eval_history, 0);
+	if (eval_history_cache == NULL) {
+		pr_warn("failed to create evaluation history cache\n");
+		return -ENOMEM;
+	}
+
+	debugfs_init();
 
 	do_test(test_alloc_release_contig);
 	do_test(test_frontswap);
