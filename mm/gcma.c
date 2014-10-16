a243a8b9a0a0aa26adb0d9fdf3cadad9e0423961
SeongJae Park <sj38.park@gmail.com>
2014-10-17 04:11:41 +0900

    gcma: introduce guaranteed contig memory allocator
    Because CMA uses page migration to enhance memory space efficiency, it
    can not guarantee success and maximum latency. This commit introduce
    Guaranteed Contiguous Memory Allocator(GCMA) which guarantees success
    and maximum latency of contiguous memory allocation.
    With first version, it just simply receive client reserved contiguous
    regions and alloc contiguous memory from the region when client request.
    This implementation can guarantee allocation success and latency because
    the area is totally reserved for contiguous memory allocation.
    But, disadvantage is obvious, too. It would use memory extremely
    inefficiently in space-point of view. The disadvantage will be conquered
    by following commit.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ mm/gcma.c
@@ -0,0 +1,219 @@
+/*
+ * gcma.c - Guaranteed Contiguous Memory Allocator
+ *
+ * GCMA aims for successful allocation within predictable time
+ *
+ * Copyright (C) 2014  Minchan Kim <minchan@kernel.org>
+ *                     SeongJae Park <sj38.park@gmail.com>
+ */
+
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/highmem.h>
+#include <linux/gcma.h>
+
+struct gcma {
+	spinlock_t lock;	/* protect bitmap */
+	unsigned long *bitmap;
+	unsigned long base_pfn;
+	unsigned long size;
+	struct list_head list;
+};
+
+struct gcma_info {
+	spinlock_t lock;	/* protect list */
+	struct list_head head;
+};
+
+static struct gcma_info ginfo = {
+	.head = LIST_HEAD_INIT(ginfo.head),
+	.lock = __SPIN_LOCK_UNLOCKED(ginfo.lock),
+};
+
+static struct gcma *find_gcma(unsigned long pfn);
+
+/** gcma_init: initializes a contiguous memory area as guaranteed
+ *
+ * @pfn		start pfn of contiguous memory area
+ * @size	size of the contiguous memory area
+ *
+ * Returns 0 if activation success, negative error code if fail
+ */
+int gcma_init(unsigned long pfn, unsigned long size)
+{
+	int bitmap_size = BITS_TO_LONGS(size) * sizeof(long);
+	struct gcma *gcma;
+
+	gcma = kmalloc(sizeof(*gcma), GFP_KERNEL);
+	if (!gcma)
+		goto out;
+
+	gcma->bitmap = kzalloc(bitmap_size, GFP_KERNEL);
+	if (!gcma->bitmap)
+		goto free_cma;
+
+	gcma->size = size;
+	gcma->base_pfn = pfn;
+	spin_lock_init(&gcma->lock);
+
+	spin_lock(&ginfo.lock);
+	list_add(&gcma->list, &ginfo.head);
+	spin_unlock(&ginfo.lock);
+
+	pr_info("gcma activate area [%lu, %lu]\n", pfn, pfn + size);
+	return 0;
+
+free_cma:
+	kfree(gcma);
+out:
+	return -ENOMEM;
+}
+
+/* Allocate a page from a gcma and return it */
+static struct page *__alloc_reclaimable(struct gcma *gcma)
+{
+	unsigned long bit;
+	unsigned long *bitmap = gcma->bitmap;
+	struct page *page = NULL;
+
+	spin_lock(&gcma->lock);
+	/* TODO: should be optimized */
+	bit = bitmap_find_next_zero_area(bitmap, gcma->size, 0, 1, 0);
+
+	if (bit >= gcma->size) {
+		spin_unlock(&gcma->lock);
+		goto out;
+	}
+
+	bitmap_set(bitmap, bit, 1);
+	page = pfn_to_page(gcma->base_pfn + bit);
+	spin_unlock(&gcma->lock);
+
+out:
+	return page;
+}
+
+/* Allocate a page from entire gcma and return it */
+__attribute__((unused))
+static struct page *alloc_reclaimable(void)
+{
+	struct page *page;
+	struct gcma *gcma;
+
+	spin_lock(&ginfo.lock);
+	gcma = list_first_entry(&ginfo.head, struct gcma, list);
+	/* Do roundrobin */
+	list_move_tail(&gcma->list, &ginfo.head);
+
+	/* Find empty slot in all gcma areas */
+	list_for_each_entry(gcma, &ginfo.head, list) {
+		page = __alloc_reclaimable(gcma);
+		if (page)
+			goto got;
+	}
+
+got:
+	spin_unlock(&ginfo.lock);
+	return page;
+}
+
+/* Free a page back to gcma */
+__attribute__((unused))
+static void gcma_free(struct page *page)
+{
+	struct gcma *gcma;
+	unsigned long pfn, offset;
+
+	pfn = page_to_pfn(page);
+	gcma = find_gcma(pfn);
+
+	spin_lock(&gcma->lock);
+	offset = pfn - gcma->base_pfn;
+
+	bitmap_clear(gcma->bitmap, offset, 1);
+	spin_unlock(&gcma->lock);
+}
+
+static struct gcma *find_gcma(unsigned long pfn)
+{
+	struct gcma *cma;
+
+	spin_lock(&ginfo.lock);
+	list_for_each_entry(cma, &ginfo.head, list) {
+		if (pfn >= cma->base_pfn && pfn < cma->base_pfn + cma->size) {
+			spin_unlock(&ginfo.lock);
+			return cma;
+		}
+	}
+
+	spin_unlock(&ginfo.lock);
+	return NULL;
+}
+
+/** gcma_alloc_contig: allocates contiguous pages
+ *
+ * @pfn		start pfn of requiring contiguous memory area
+ * @size	size of the requiring contiguous memory area
+ *
+ * Returns 0 if activation success, negative error code if fail
+ */
+int gcma_alloc_contig(unsigned long start, unsigned long end)
+{
+	struct gcma *gcma;
+	unsigned long offset;
+	unsigned long nr_pages;
+
+	nr_pages = end - start;
+	gcma = find_gcma(start);
+	if (!gcma)
+		return -EINVAL;
+
+	spin_lock(&gcma->lock);
+	offset = start - gcma->base_pfn;
+
+	if (bitmap_find_next_zero_area(gcma->bitmap, gcma->size, offset,
+				nr_pages, 0) != 0) {
+		spin_unlock(&gcma->lock);
+		return -EINVAL;
+	}
+
+	bitmap_set(gcma->bitmap, offset, end - start);
+	spin_unlock(&gcma->lock);
+
+	return 0;
+}
+
+/** gcma_free_contig: free allocated contiguous pages
+ *
+ * @pfn		start pfn of requiring contiguous memory area
+ * @size	size of the requiring contiguous memory area
+ */
+void gcma_free_contig(unsigned long pfn, unsigned long nr_pages)
+{
+	struct gcma *gcma;
+	unsigned long offset;
+
+	gcma = find_gcma(pfn);
+
+	spin_lock(&gcma->lock);
+	offset = pfn - gcma->base_pfn;
+	bitmap_clear(gcma->bitmap, offset, nr_pages);
+
+	spin_unlock(&gcma->lock);
+}
+
+static int __init init_gcma(void)
+{
+	pr_info("loading gcma\n");
+
+	return 0;
+}
+
+module_init(init_gcma);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Minchan Kim <minchan@kernel.org>");
+MODULE_AUTHOR("SeongJae Park <sj38.park@gmail.com>");
+MODULE_DESCRIPTION("Guaranteed Contiguous Memory Allocator");
