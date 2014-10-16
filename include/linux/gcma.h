8e3e9bfbe79ffef4bfb84529898cbc4396d5d302
SeongJae Park <sj38.park@gmail.com>
2014-10-17 04:11:41 +0900

    gcma: introduce contiguous memory allocator
    This patch introduces a simple contiguous memory allocator. It just
    receive few contiguous memory regions and allocate contiguous pages
    from the region by managing the pages using bitmap.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ include/linux/gcma.h
@@ -0,0 +1,35 @@
+/*
+ * gcma.h - Guaranteed Contiguous Memory Allocator
+ *
+ * gcma aims for contiguous memory allocation with success and fast
+ * latency guarantee.
+ * It reserve large amount of memory and let it be allocated to the
+ * contiguous memory request.
+ *
+ * Copyright (C) 2014  Minchan Kim <minchan@kernel.org>
+ *                     SeongJae Park <sj38.park@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _LINUX_GCMA_H
+#define _LINUX_GCMA_H
+
+struct gcma;
+
+int gcma_init(unsigned long start_pfn, unsigned long size,
+	      struct gcma **res_gcma);
+int gcma_alloc_contig(struct gcma *gcma,
+		      unsigned long start_pfn, unsigned long size);
+void gcma_free_contig(struct gcma *gcma,
+		      unsigned long start_pfn, unsigned long size);
+
+#endif /* _LINUX_GCMA_H */
