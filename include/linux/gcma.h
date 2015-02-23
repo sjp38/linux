f1f04bca1312d1a4de3e68baa788fa9d81057b6c
SeongJae Park <sj38.park@gmail.com>
2015-02-24 01:18:28 +0900

    gcma: utilize gcma area for cleancache and frontswap backend
    Because pages in cleancache is clean and out of kernel scope, they could
    be free immediately whenever it required to be with no additional task.
    Similarly, because frontswap pages are out of kernel scope, too, they
    could be free easily after written back to backing swap device.
    Moreover, the writing back task could be avoided if frontswap works as
    write-through mode.  It means cleancache and write-through mode
    frontswap pages are best candidates for second-class clients of gcma.
    
    By the consequence, this commit implements cleancache and write-through
    mode frontswap backend inside gcma area using dmem interface.
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- include/linux/gcma.h
+++ include/linux/gcma.h
@@ -15,6 +15,9 @@
  * restricts only easily discardable memory could be used for second-class
  * clients.
  *
+ * Currently, GCMA uses cleancache and write-through mode frontswap as
+ * second-class clients.
+ *
  * Copyright (C) 2014  LG Electronics Inc.,
  * Copyright (C) 2014  Minchan Kim <minchan@kernel.org>
  * Copyright (C) 2014-2015  SeongJae Park <sj38.park@gmail.com>
