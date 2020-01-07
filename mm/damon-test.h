be83fa59275100c5f4a644a8dffbeb9d70ba871e
SeongJae Park <sjpark@amazon.de>
2020-01-07 14:12:16 +0000

    damon-test: Fix 'checkpatch.pl' warnings
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- mm/damon-test.h
+++ mm/damon-test.h
@@ -23,51 +23,51 @@ static void damon_test_str_to_pids(struct kunit *test)
 
 	question = "123";
 	answers = str_to_pids(question, strnlen(question, 128), &nr_integers);
-	KUNIT_EXPECT_EQ(test, (ssize_t)1, nr_integers);
-	KUNIT_EXPECT_EQ(test, (unsigned long)123, answers[0]);
+	KUNIT_EXPECT_EQ(test, 1l, nr_integers);
+	KUNIT_EXPECT_EQ(test, 123ul, answers[0]);
 	kfree(answers);
 
 	question = "123abc";
 	answers = str_to_pids(question, strnlen(question, 128), &nr_integers);
-	KUNIT_EXPECT_EQ(test, (ssize_t)1, nr_integers);
-	KUNIT_EXPECT_TRUE(test, 123 == answers[0]);
+	KUNIT_EXPECT_EQ(test, 1l, nr_integers);
+	KUNIT_EXPECT_EQ(test, 123ul, answers[0]);
 	kfree(answers);
 
 	question = "a123";
 	answers = str_to_pids(question, strnlen(question, 128), &nr_integers);
-	KUNIT_EXPECT_TRUE(test, nr_integers == 0);
-	KUNIT_EXPECT_TRUE(test, answers == NULL);
+	KUNIT_EXPECT_EQ(test, 0l, nr_integers);
+	KUNIT_EXPECT_PTR_EQ(test, (void *)answers, NULL);
 
 	question = "12 35";
 	answers = str_to_pids(question, strnlen(question, 128), &nr_integers);
-	KUNIT_EXPECT_EQ(test, (ssize_t)2, nr_integers);
+	KUNIT_EXPECT_EQ(test, 2l, nr_integers);
 	for (i = 0; i < nr_integers; i++)
 		KUNIT_EXPECT_EQ(test, expected[i], answers[i]);
 	kfree(answers);
 
 	question = "12 35 46";
 	answers = str_to_pids(question, strnlen(question, 128), &nr_integers);
-	KUNIT_EXPECT_EQ(test, (ssize_t)3, nr_integers);
+	KUNIT_EXPECT_EQ(test, 3l, nr_integers);
 	for (i = 0; i < nr_integers; i++)
 		KUNIT_EXPECT_EQ(test, expected[i], answers[i]);
 	kfree(answers);
 
 	question = "12 35 abc 46";
 	answers = str_to_pids(question, strnlen(question, 128), &nr_integers);
-	KUNIT_EXPECT_EQ(test, (ssize_t)2, nr_integers);
+	KUNIT_EXPECT_EQ(test, 2l, nr_integers);
 	for (i = 0; i < 2; i++)
 		KUNIT_EXPECT_EQ(test, expected[i], answers[i]);
 	kfree(answers);
 
 	question = "";
 	answers = str_to_pids(question, strnlen(question, 128), &nr_integers);
-	KUNIT_EXPECT_EQ(test, (ssize_t)0, nr_integers);
+	KUNIT_EXPECT_EQ(test, 0l, nr_integers);
 	KUNIT_EXPECT_PTR_EQ(test, (unsigned long *)NULL, answers);
 	kfree(answers);
 
 	question = "\n";
 	answers = str_to_pids(question, strnlen(question, 128), &nr_integers);
-	KUNIT_EXPECT_EQ(test, (ssize_t)0, nr_integers);
+	KUNIT_EXPECT_EQ(test, 0l, nr_integers);
 	KUNIT_EXPECT_PTR_EQ(test, (unsigned long *)NULL, answers);
 	kfree(answers);
 }
@@ -78,20 +78,20 @@ static void damon_test_regions(struct kunit *test)
 	struct damon_task *t;
 
 	r = damon_new_region(1, 2);
-	KUNIT_EXPECT_EQ(test, (unsigned long)1, r->vm_start);
-	KUNIT_EXPECT_EQ(test, (unsigned long)2, r->vm_end);
-	KUNIT_EXPECT_EQ(test, (unsigned int)0, r->nr_accesses);
+	KUNIT_EXPECT_EQ(test, 1ul, r->vm_start);
+	KUNIT_EXPECT_EQ(test, 2ul, r->vm_end);
+	KUNIT_EXPECT_EQ(test, 0u, r->nr_accesses);
 	KUNIT_EXPECT_TRUE(test, r->sampling_addr >= r->vm_start);
 	KUNIT_EXPECT_TRUE(test, r->sampling_addr < r->vm_end);
 
 	t = damon_new_task(42);
-	KUNIT_EXPECT_EQ(test, (unsigned int)0, nr_damon_regions(t));
+	KUNIT_EXPECT_EQ(test, 0u, nr_damon_regions(t));
 
 	damon_add_region_tail(r, t);
-	KUNIT_EXPECT_EQ(test, (unsigned int)1, nr_damon_regions(t));
+	KUNIT_EXPECT_EQ(test, 1u, nr_damon_regions(t));
 
 	damon_del_region(r);
-	KUNIT_EXPECT_EQ(test, (unsigned int)0, nr_damon_regions(t));
+	KUNIT_EXPECT_EQ(test, 0u, nr_damon_regions(t));
 
 	damon_free_task(t);
 }
@@ -101,14 +101,14 @@ static void damon_test_tasks(struct kunit *test)
 	struct damon_task *t;
 
 	t = damon_new_task(42);
-	KUNIT_EXPECT_EQ(test, (unsigned long)42, t->pid);
-	KUNIT_EXPECT_EQ(test, (unsigned int)0, nr_damon_tasks());
+	KUNIT_EXPECT_EQ(test, 42ul, t->pid);
+	KUNIT_EXPECT_EQ(test, 0u, nr_damon_tasks());
 
 	damon_add_task_tail(t);
-	KUNIT_EXPECT_EQ(test, (unsigned int)1, nr_damon_tasks());
+	KUNIT_EXPECT_EQ(test, 1u, nr_damon_tasks());
 
 	damon_destroy_task(t);
-	KUNIT_EXPECT_EQ(test, (unsigned int)0, nr_damon_tasks());
+	KUNIT_EXPECT_EQ(test, 0u, nr_damon_tasks());
 }
 
 static void damon_test_set_pids(struct kunit *test)
@@ -159,12 +159,12 @@ static void damon_test_three_regions_in_vmas(struct kunit *test)
 
 	damon_three_regions_in_vmas(&vmas[0], regions);
 
-	KUNIT_EXPECT_EQ(test, (unsigned long)10, regions[0].start);
-	KUNIT_EXPECT_EQ(test, (unsigned long)25, regions[0].end);
-	KUNIT_EXPECT_EQ(test, (unsigned long)200, regions[1].start);
-	KUNIT_EXPECT_EQ(test, (unsigned long)220, regions[1].end);
-	KUNIT_EXPECT_EQ(test, (unsigned long)300, regions[2].start);
-	KUNIT_EXPECT_EQ(test, (unsigned long)330, regions[2].end);
+	KUNIT_EXPECT_EQ(test, 10ul, regions[0].start);
+	KUNIT_EXPECT_EQ(test, 25ul, regions[0].end);
+	KUNIT_EXPECT_EQ(test, 200ul, regions[1].start);
+	KUNIT_EXPECT_EQ(test, 220ul, regions[1].end);
+	KUNIT_EXPECT_EQ(test, 300ul, regions[2].start);
+	KUNIT_EXPECT_EQ(test, 330ul, regions[2].end);
 }
 
 /* Clean up global state of damon */
@@ -205,7 +205,7 @@ static void damon_test_aggregate(struct kunit *test)
 	damon_for_each_task(t) {
 		ir = 0;
 		damon_for_each_region(r, t) {
-			KUNIT_EXPECT_EQ(test, (unsigned int)0, r->nr_accesses);
+			KUNIT_EXPECT_EQ(test, 0u, r->nr_accesses);
 			ir++;
 		}
 		KUNIT_EXPECT_EQ(test, 3, ir);
@@ -227,10 +227,10 @@ static void damon_test_write_rbuf(struct kunit *test)
 
 	data = "hello";
 	damon_write_rbuf(data, strnlen(data, 256));
-	KUNIT_EXPECT_EQ(test, damon_rbuf_offset, (unsigned int)5);
+	KUNIT_EXPECT_EQ(test, damon_rbuf_offset, 5u);
 
 	damon_write_rbuf(data, 0);
-	KUNIT_EXPECT_EQ(test, damon_rbuf_offset, (unsigned int)5);
+	KUNIT_EXPECT_EQ(test, damon_rbuf_offset, 5u);
 
 	KUNIT_EXPECT_EQ(test, strncmp(damon_rbuf, data, 5), 0);
 }
@@ -260,20 +260,20 @@ static void damon_test_update_two_gaps(struct kunit *test)
 
 	damon_for_each_region(r, t) {
 		if (prev == NULL) {
-			KUNIT_EXPECT_EQ(test, r->vm_start, (unsigned long)5);
+			KUNIT_EXPECT_EQ(test, r->vm_start, 5ul);
 			goto next;
 		}
 
 		if (prev->vm_end != r->vm_start && first_gap) {
-			KUNIT_EXPECT_EQ(test, prev->vm_end, (unsigned long)27);
-			KUNIT_EXPECT_EQ(test, r->vm_start, (unsigned long)45);
+			KUNIT_EXPECT_EQ(test, prev->vm_end, 27ul);
+			KUNIT_EXPECT_EQ(test, r->vm_start, 45ul);
 			first_gap = false;
 			goto next;
 		}
 
 		if (prev->vm_end != r->vm_start && !first_gap) {
-			KUNIT_EXPECT_EQ(test, prev->vm_end, (unsigned long)55);
-			KUNIT_EXPECT_EQ(test, r->vm_start, (unsigned long)73);
+			KUNIT_EXPECT_EQ(test, prev->vm_end, 55ul);
+			KUNIT_EXPECT_EQ(test, r->vm_start, 73ul);
 			goto next;
 		}
 
@@ -402,7 +402,7 @@ static void damon_test_split(struct kunit *test)
 
 	damon_add_region_tail(r, t);
 	KUNIT_EXPECT_EQ(test, damon_split_region(r, 10), 0);
-	KUNIT_EXPECT_EQ(test, nr_damon_regions(t), (unsigned int)10);
+	KUNIT_EXPECT_EQ(test, nr_damon_regions(t), 10u);
 
 	i = 0;
 	damon_for_each_region(r, t) {
@@ -415,7 +415,7 @@ static void damon_test_split(struct kunit *test)
 	r = damon_new_region(5, 59);
 	damon_add_region_tail(r, t);
 	KUNIT_EXPECT_EQ(test, damon_split_region(r, 5), 0);
-	KUNIT_EXPECT_EQ(test, nr_damon_regions(t), (unsigned int)5);
+	KUNIT_EXPECT_EQ(test, nr_damon_regions(t), 5u);
 
 	i = 0;
 	damon_for_each_region(r, t) {
@@ -425,18 +425,18 @@ static void damon_test_split(struct kunit *test)
 		KUNIT_EXPECT_EQ(test, r->vm_end, 5 + 10 * i);
 	}
 	KUNIT_EXPECT_EQ(test, r->vm_start, 5 + 10 * i);
-	KUNIT_EXPECT_EQ(test, r->vm_end, (unsigned long)59);
+	KUNIT_EXPECT_EQ(test, r->vm_end, 59ul);
 	damon_free_task(t);
 
 	t = damon_new_task(42);
 	r = damon_new_region(5, 6);
 	damon_add_region_tail(r, t);
 	KUNIT_EXPECT_EQ(test, damon_split_region(r, 2), -EINVAL);
-	KUNIT_EXPECT_EQ(test, nr_damon_regions(t), (unsigned int)1);
+	KUNIT_EXPECT_EQ(test, nr_damon_regions(t), 1u);
 
 	damon_for_each_region(r, t) {
-		KUNIT_EXPECT_EQ(test, r->vm_start, (unsigned long)5);
-		KUNIT_EXPECT_EQ(test, r->vm_end, (unsigned long)6);
+		KUNIT_EXPECT_EQ(test, r->vm_start, 5ul);
+		KUNIT_EXPECT_EQ(test, r->vm_end, 6ul);
 	}
 	damon_free_task(t);
 }
@@ -450,12 +450,12 @@ static void damon_test_split_at(struct kunit *test)
 	r = damon_new_region(0, 100);
 	damon_add_region_tail(r, t);
 	damon_split_region_at(r, 25);
-	KUNIT_EXPECT_EQ(test, r->vm_start, (unsigned long)0);
-	KUNIT_EXPECT_EQ(test, r->vm_end, (unsigned long)25);
+	KUNIT_EXPECT_EQ(test, r->vm_start, 0ul);
+	KUNIT_EXPECT_EQ(test, r->vm_end, 25ul);
 
 	r = damon_next_region(r);
-	KUNIT_EXPECT_EQ(test, r->vm_start, (unsigned long)25);
-	KUNIT_EXPECT_EQ(test, r->vm_end, (unsigned long)100);
+	KUNIT_EXPECT_EQ(test, r->vm_start, 25ul);
+	KUNIT_EXPECT_EQ(test, r->vm_end, 100ul);
 
 	damon_free_task(t);
 }
@@ -475,9 +475,9 @@ static void damon_test_merge_two(struct kunit *test)
 	damon_add_region_tail(r2, t);
 
 	damon_merge_two_regions(r, r2);
-	KUNIT_EXPECT_EQ(test, r->vm_start, (unsigned long)0);
-	KUNIT_EXPECT_EQ(test, r->vm_end, (unsigned long)300);
-	KUNIT_EXPECT_EQ(test, r->nr_accesses, (unsigned int)16);
+	KUNIT_EXPECT_EQ(test, r->vm_start, 0ul);
+	KUNIT_EXPECT_EQ(test, r->vm_end, 300ul);
+	KUNIT_EXPECT_EQ(test, r->nr_accesses, 16u);
 
 	i = 0;
 	damon_for_each_region(r3, t) {
@@ -510,7 +510,7 @@ static void damon_test_merge_regions_of(struct kunit *test)
 
 	damon_merge_regions_of(t, 9);
 	/* 0-112, 114-130, 130-156, 156-170 */
-	KUNIT_EXPECT_EQ(test, nr_damon_regions(t), (unsigned int)5);
+	KUNIT_EXPECT_EQ(test, nr_damon_regions(t), 5u);
 	for (i = 0; i < 5; i++) {
 		r = damon_nth_region_of(t, i);
 		KUNIT_EXPECT_EQ(test, r->vm_start, saddrs[i]);
@@ -528,7 +528,7 @@ static void damon_test_split_regions_of(struct kunit *test)
 	r = damon_new_region(0, 22);
 	damon_add_region_tail(r, t);
 	damon_split_regions_of(t);
-	KUNIT_EXPECT_EQ(test, nr_damon_regions(t), (unsigned int)2);
+	KUNIT_EXPECT_EQ(test, nr_damon_regions(t), 2u);
 	damon_free_task(t);
 }
 
