b894d539fa9e65f17ed121768086b5a040b288b2
SeongJae Park <sjpark@amazon.de>
2020-02-02 21:52:42 +0000

    mm: Introduce Data Access MONitor (DAMON)
    DAMON is a data access monitoring framework subsystem for the Linux
    kernel.  The core mechanisms of DAMON make it
    
     - accurate (the monitoring output is useful enough for DRAM level
       memory management; It might not appropriate for CPU Cache levels,
       though),
     - light-weight (the monitoring overhead is low enough to be applied
       online), and
     - scalable (the upper-bound of the overhead is in constant range
       regardless of the size of target workloads).
    
    Using this framework, therefore, the kernel's memory management
    mechanisms can make advanced decisions.  Experimental memory management
    optimization works that incurring high data accesses monitoring overhead
    could implemented again.  In user space, meanwhile, users who have some
    special workloads can write personalized applications for better
    understanding and optimizations of their workloads and systems.
    
    This commit is implementing only the stub for the initialization, basic
    data structures, and simple manipulation functions of the structures.
    The core mechanisms of DAMON will be implemented by following commits.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>
    Reviewed-by: Leonard Foerster <foersleo@amazon.de>
    Reviewed-by: Varad Gautam <vrd@amazon.de>

---
+++ include/linux/damon.h
@@ -0,0 +1,66 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * DAMON api
+ *
+ * Copyright 2019-2020 Amazon.com, Inc. or its affiliates.
+ *
+ * Author: SeongJae Park <sjpark@amazon.de>
+ */
+
+#ifndef _DAMON_H_
+#define _DAMON_H_
+
+#include <linux/random.h>
+#include <linux/types.h>
+
+/**
+ * struct damon_addr_range - Represents an address region of [@start, @end).
+ * @start:	Start address of the region (inclusive).
+ * @end:	End address of the region (exclusive).
+ */
+struct damon_addr_range {
+	unsigned long start;
+	unsigned long end;
+};
+
+/**
+ * struct damon_region - Represents a monitoring target region.
+ * @ar:			The address range of the region.
+ * @sampling_addr:	Address of the sample for the next access check.
+ * @nr_accesses:	Access frequency of this region.
+ * @list:		List head for siblings.
+ */
+struct damon_region {
+	struct damon_addr_range ar;
+	unsigned long sampling_addr;
+	unsigned int nr_accesses;
+	struct list_head list;
+};
+
+/**
+ * struct damon_target - Represents a monitoring target.
+ * @id:			Unique identifier for this target.
+ * @regions_list:	Head of the monitoring target regions of this target.
+ * @list:		List head for siblings.
+ *
+ * Each monitoring context could have multiple targets.  For example, a context
+ * for virtual memory address spaces could have multiple target processes.  The
+ * @id of each target should be unique among the targets of the context.  For
+ * example, in the virtual address monitoring context, it could be a pidfd or
+ * an adress of an mm_struct.
+ */
+struct damon_target {
+	unsigned long id;
+	struct list_head regions_list;
+	struct list_head list;
+};
+
+/**
+ * struct damon_ctx - Represents a context for each monitoring.
+ * @targets_list:	Head of monitoring targets (&damon_target) list.
+ */
+struct damon_ctx {
+	struct list_head targets_list;	/* 'damon_target' objects */
+};
+
+#endif
