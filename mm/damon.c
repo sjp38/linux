3f3463c82d08f0b908a341511a6dd0871e638c19
SeongJae Park <sjpark@amazon.de>
2020-03-27 12:52:45 +0100

    mm/damon: Use vm-independent address range concept
    DAMON's main idea is not limited to virtual address space.  To prepare
    for further expansion of the support for other address spaces including
    physical memory, this commit modifies one of its core struct, 'struct
    damon_region' to use virtual memory independent address space concept.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- mm/damon.c
+++ mm/damon.c
@@ -75,7 +75,7 @@ static struct damon_ctx damon_user_ctx = {
  * Returns the pointer to the new struct if success, or NULL otherwise
  */
 static struct damon_region *damon_new_region(struct damon_ctx *ctx,
-				unsigned long vm_start, unsigned long vm_end)
+				unsigned long start, unsigned long end)
 {
 	struct damon_region *region;
 
@@ -83,14 +83,14 @@ static struct damon_region *damon_new_region(struct damon_ctx *ctx,
 	if (!region)
 		return NULL;
 
-	region->vm_start = vm_start;
-	region->vm_end = vm_end;
+	region->ar.start = start;
+	region->ar.end = end;
 	region->nr_accesses = 0;
 	INIT_LIST_HEAD(&region->list);
 
 	region->age = 0;
-	region->last_vm_start = vm_start;
-	region->last_vm_end = vm_end;
+	region->last_ar.start = start;
+	region->last_ar.end = end;
 
 	return region;
 }
@@ -285,16 +285,16 @@ static int damon_split_region_evenly(struct damon_ctx *ctx,
 	if (!r || !nr_pieces)
 		return -EINVAL;
 
-	orig_end = r->vm_end;
-	sz_orig = r->vm_end - r->vm_start;
+	orig_end = r->ar.end;
+	sz_orig = r->ar.end - r->ar.start;
 	sz_piece = sz_orig / nr_pieces;
 
 	if (!sz_piece)
 		return -EINVAL;
 
-	r->vm_end = r->vm_start + sz_piece;
+	r->ar.end = r->ar.start + sz_piece;
 	next = damon_next_region(r);
-	for (start = r->vm_end; start + sz_piece <= orig_end;
+	for (start = r->ar.end; start + sz_piece <= orig_end;
 			start += sz_piece) {
 		n = damon_new_region(ctx, start, start + sz_piece);
 		damon_insert_region(n, r, next);
@@ -302,7 +302,7 @@ static int damon_split_region_evenly(struct damon_ctx *ctx,
 	}
 	/* complement last region for possible rounding error */
 	if (n)
-		n->vm_end = orig_end;
+		n->ar.end = orig_end;
 
 	return 0;
 }
@@ -488,7 +488,7 @@ static void kdamond_prepare_sampling(struct damon_ctx *ctx,
 	unsigned long tlb_start;
 	unsigned long tlb_size = PAGE_SIZE;
 
-	r->sampling_addr = damon_rand(ctx, r->vm_start, r->vm_end);
+	r->sampling_addr = damon_rand(ctx, r->ar.start, r->ar.end);
 
 	if (follow_pte_pmd(mm, r->sampling_addr, NULL, &pte, &pmd, &ptl))
 		return;
@@ -682,12 +682,12 @@ static void kdamond_reset_aggregated(struct damon_ctx *c)
 		nr = nr_damon_regions(t);
 		damon_write_rbuf(c, &nr, sizeof(nr));
 		damon_for_each_region(r, t) {
-			damon_write_rbuf(c, &r->vm_start, sizeof(r->vm_start));
-			damon_write_rbuf(c, &r->vm_end, sizeof(r->vm_end));
+			damon_write_rbuf(c, &r->ar.start, sizeof(r->ar.start));
+			damon_write_rbuf(c, &r->ar.end, sizeof(r->ar.end));
 			damon_write_rbuf(c, &r->nr_accesses,
 					sizeof(r->nr_accesses));
 			trace_damon_aggregated(t->pid, nr,
-					r->vm_start, r->vm_end, r->nr_accesses);
+					r->ar.start, r->ar.end, r->nr_accesses);
 			r->last_nr_accesses = r->nr_accesses;
 			r->nr_accesses = 0;
 		}
@@ -704,10 +704,10 @@ static void kdamond_reset_aggregated(struct damon_ctx *c)
  */
 static void damon_do_count_age(struct damon_region *r, unsigned int threshold)
 {
-	unsigned long sz_threshold = (r->vm_end - r->vm_start) / 5;
+	unsigned long sz_threshold = (r->ar.end - r->ar.start) / 5;
 
-	if (diff_of(r->vm_start, r->last_vm_start) +
-			diff_of(r->vm_end, r->last_vm_end) > sz_threshold)
+	if (diff_of(r->ar.start, r->last_ar.start) +
+			diff_of(r->ar.end, r->last_ar.end) > sz_threshold)
 		r->age = 0;
 	else if (diff_of(r->nr_accesses, r->last_nr_accesses) > threshold)
 		r->age = 0;
@@ -749,8 +749,8 @@ static int damos_madvise(struct damon_task *task, struct damon_region *r,
 	if (!mm)
 		goto put_task_out;
 
-	ret = do_madvise(t, mm, PAGE_ALIGN(r->vm_start),
-			PAGE_ALIGN(r->vm_end - r->vm_start), behavior);
+	ret = do_madvise(t, mm, PAGE_ALIGN(r->ar.start),
+			PAGE_ALIGN(r->ar.end - r->ar.start), behavior);
 	mmput(mm);
 put_task_out:
 	put_task_struct(t);
@@ -796,7 +796,7 @@ static void damon_do_apply_schemes(struct damon_ctx *c, struct damon_task *t,
 	unsigned long sz;
 
 	damon_for_each_schemes(c, s) {
-		sz = r->vm_end - r->vm_start;
+		sz = r->ar.end - r->ar.start;
 		if ((s->min_sz_region && sz < s->min_sz_region) ||
 				(s->max_sz_region && s->max_sz_region < sz))
 			continue;
@@ -824,7 +824,7 @@ static void kdamond_apply_schemes(struct damon_ctx *c)
 	}
 }
 
-#define sz_damon_region(r) (r->vm_end - r->vm_start)
+#define sz_damon_region(r) (r->ar.end - r->ar.start)
 
 /*
  * Merge two adjacent regions into one region
@@ -837,20 +837,20 @@ static void damon_merge_two_regions(struct damon_region *l,
 	l->nr_accesses = (l->nr_accesses * sz_l + r->nr_accesses * sz_r) /
 			(sz_l + sz_r);
 	l->age = (l->age * sz_l + r->age * sz_r) / (sz_l + sz_r);
-	l->vm_end = r->vm_end;
+	l->ar.end = r->ar.end;
 	damon_destroy_region(r);
 }
 
 static inline void set_last_area(struct damon_region *r, struct region *last)
 {
-	r->last_vm_start = last->start;
-	r->last_vm_end = last->end;
+	r->last_ar.start = last->start;
+	r->last_ar.end = last->end;
 }
 
 static inline void get_last_area(struct damon_region *r, struct region *last)
 {
-	last->start = r->last_vm_start;
-	last->end = r->last_vm_end;
+	last->start = r->last_ar.start;
+	last->end = r->last_ar.end;
 }
 
 /*
@@ -882,7 +882,7 @@ static void damon_merge_regions_of(struct damon_task *t, unsigned int thres)
 	unsigned long sz_mergee = 0;	/* size of current mergee */
 
 	damon_for_each_region_safe(r, next, t) {
-		if (!prev || prev->vm_end != r->vm_start ||
+		if (!prev || prev->ar.end != r->ar.start ||
 		    diff_of(prev->nr_accesses, r->nr_accesses) > thres) {
 			if (sz_biggest)
 				set_last_area(prev, &biggest_mergee);
@@ -905,7 +905,7 @@ static void damon_merge_regions_of(struct damon_task *t, unsigned int thres)
 		 * If next region and current region is not originated from
 		 * same region, initialize the size of mergee.
 		 */
-		if (r->last_vm_start != next->last_vm_start)
+		if (r->last_ar.start != next->last_ar.start)
 			sz_mergee = 0;
 
 		damon_merge_two_regions(prev, r);
@@ -943,14 +943,14 @@ static void damon_split_region_at(struct damon_ctx *ctx,
 {
 	struct damon_region *new;
 
-	new = damon_new_region(ctx, r->vm_start + sz_r, r->vm_end);
+	new = damon_new_region(ctx, r->ar.start + sz_r, r->ar.end);
 	new->age = r->age;
-	new->last_vm_start = r->vm_start;
+	new->last_ar.start = r->ar.start;
 	new->last_nr_accesses = r->last_nr_accesses;
 
-	r->last_vm_start = r->vm_start;
-	r->last_vm_end = r->vm_end;
-	r->vm_end = new->vm_start;
+	r->last_ar.start = r->ar.start;
+	r->last_ar.end = r->ar.end;
+	r->ar.end = new->ar.start;
 
 	damon_insert_region(new, r, damon_next_region(r));
 }
@@ -966,7 +966,7 @@ static void damon_split_regions_of(struct damon_ctx *ctx, struct damon_task *t)
 		 * 10 percent and at most 90% of original region
 		 */
 		sz_left_region = (prandom_u32_state(&ctx->rndseed) % 9 + 1) *
-			(r->vm_end - r->vm_start) / 10;
+			(r->ar.end - r->ar.start) / 10;
 		/* Do not allow blank region */
 		if (sz_left_region == 0)
 			continue;
@@ -1011,7 +1011,7 @@ static bool kdamond_need_update_regions(struct damon_ctx *ctx)
 
 static bool damon_intersect(struct damon_region *r, struct region *re)
 {
-	return !(r->vm_end <= re->start || re->end <= r->vm_start);
+	return !(r->ar.end <= re->start || re->end <= r->ar.start);
 }
 
 /*
@@ -1050,7 +1050,7 @@ static void damon_apply_three_regions(struct damon_ctx *ctx,
 					first = r;
 				last = r;
 			}
-			if (r->vm_start >= br->end)
+			if (r->ar.start >= br->end)
 				break;
 		}
 		if (!first) {
@@ -1058,8 +1058,8 @@ static void damon_apply_three_regions(struct damon_ctx *ctx,
 			newr = damon_new_region(ctx, br->start, br->end);
 			damon_insert_region(newr, damon_prev_region(r), r);
 		} else {
-			first->vm_start = br->start;
-			last->vm_end = br->end;
+			first->ar.start = br->start;
+			last->ar.end = br->end;
 		}
 	}
 }
