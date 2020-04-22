e4ed27003b186ef119060a684f20e6865cc58363
SeongJae Park <sjpark@amazon.de>
2020-04-22 09:07:13 +0000

    mm/damon: Implement region based sampling
    This commit implements DAMON's basic access check and region based
    sampling mechanisms.  This change would seems make no sense, mainly
    because it is only a part of the DAMON's logics.  Following two commits
    will make more sense.
    
    Basic Access Check
    ------------------
    
    DAMON basically reports what pages are how frequently accessed.  Note
    that the frequency is not an absolute number of accesses, but a relative
    frequency among the pages of the target workloads.
    
    Users can control the resolution of the reports by setting two time
    intervals, ``sampling interval`` and ``aggregation interval``.  In
    detail, DAMON checks access to each page per ``sampling interval``,
    aggregates the results (counts the number of the accesses to each page),
    and reports the aggregated results per ``aggregation interval``.  For
    the access check of each page, DAMON uses the Accessed bits of PTEs.
    
    This is thus similar to common periodic access checks based access
    tracking mechanisms, which overhead is increasing as the size of the
    target process grows.
    
    Region Based Sampling
    ---------------------
    
    To avoid the unbounded increase of the overhead, DAMON groups a number
    of adjacent pages that assumed to have same access frequencies into a
    region.  As long as the assumption (pages in a region have same access
    frequencies) is kept, only one page in the region is required to be
    checked.  Thus, for each ``sampling interval``, DAMON randomly picks one
    page in each region and clears its Accessed bit.  After one more
    ``sampling interval``, DAMON reads the Accessed bit of the page and
    increases the access frequency of the region if the bit has set
    meanwhile.  Therefore, the monitoring overhead is controllable by
    setting the number of regions.
    
    Nonetheless, this scheme cannot preserve the quality of the output if
    the assumption is not kept.  Following commit will introduce how we can
    make the guarantee with best effort.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -11,6 +11,8 @@
 #define _DAMON_H_
 
 #include <linux/random.h>
+#include <linux/mutex.h>
+#include <linux/time64.h>
 #include <linux/types.h>
 
 /* Represents a monitoring target region of [vm_start, vm_end) */
@@ -29,8 +31,30 @@ struct damon_task {
 	struct list_head list;
 };
 
+/*
+ * For each 'sample_interval', DAMON checks whether each region is accessed or
+ * not.  It aggregates and keeps the access information (number of accesses to
+ * each region) for each 'aggr_interval' time.
+ *
+ * All time intervals are in micro-seconds.
+ */
 struct damon_ctx {
+	unsigned long sample_interval;
+	unsigned long aggr_interval;
+	unsigned long min_nr_regions;
+
+	struct timespec64 last_aggregation;
+
+	struct task_struct *kdamond;
+	struct mutex kdamond_lock;
+
 	struct list_head tasks_list;	/* 'damon_task' objects */
 };
+
+int damon_set_pids(struct damon_ctx *ctx, unsigned long *pids, ssize_t nr_pids);
+int damon_set_attrs(struct damon_ctx *ctx, unsigned long sample_int,
+		unsigned long aggr_int, unsigned long min_nr_reg);
+int damon_start(struct damon_ctx *ctx);
+int damon_stop(struct damon_ctx *ctx);
 
 #endif
