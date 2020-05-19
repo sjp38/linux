0a8a0a7c163e134c1073e3de08ad90785ae27d89
SeongJae Park <sjpark@amazon.de>
2020-05-19 12:05:35 +0000

    mm/damon/schemes: Implement statistics feature
    To tune the DAMON-based operation schemes, knowing how many and how
    large regions are affected by each of the schemes will be helful.  Those
    stats could be used for not only the tuning, but also monitoring of the
    working set size and the number of regions, if the scheme does not
    change the program behavior too much.
    
    For the reason, this commit implements the statistics for the schemes.
    The total number and size of the regions that each scheme is applied are
    exported to users via '->stat_count' and '->stat_sz' of 'struct damos'.
    Admins can also check the number by reading 'schemes' debugfs file.  The
    last two integers now represents the stats.  To allow collecting the
    stats without changing the program behavior, this commit also adds new
    scheme action, 'DAMOS_STAT'.  Note that 'DAMOS_STAT' is not only making
    no memory operation actions, but also does not reset the age of regions.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- mm/damon.c
+++ mm/damon.c
@@ -212,6 +212,8 @@ static struct damos *damon_new_scheme(
 	scheme->min_age_region = min_age_region;
 	scheme->max_age_region = max_age_region;
 	scheme->action = action;
+	scheme->stat_count = 0;
+	scheme->stat_sz = 0;
 	INIT_LIST_HEAD(&scheme->list);
 
 	return scheme;
@@ -987,6 +989,8 @@ static int damos_do_action(struct damon_target *target, struct damon_region *r,
 	case DAMOS_NOHUGEPAGE:
 		madv_action = MADV_NOHUGEPAGE;
 		break;
+	case DAMOS_STAT:
+		return 0;
 	default:
 		pr_warn("Wrong action %d\n", action);
 		return -EINVAL;
@@ -1011,8 +1015,11 @@ static void damon_do_apply_schemes(struct damon_ctx *c,
 			continue;
 		if (r->age < s->min_age_region || s->max_age_region < r->age)
 			continue;
+		s->stat_count++;
+		s->stat_sz += sz;
 		damos_do_action(t, r, s->action);
-		r->age = 0;
+		if (s->action != DAMOS_STAT)
+			r->age = 0;
 	}
 }
 
@@ -1565,11 +1572,11 @@ static ssize_t sprint_schemes(struct damon_ctx *c, char *buf, ssize_t len)
 
 	damon_for_each_scheme(s, c) {
 		rc = snprintf(&buf[written], len - written,
-				"%lu %lu %u %u %u %u %d\n",
+				"%lu %lu %u %u %u %u %d %lu %lu\n",
 				s->min_sz_region, s->max_sz_region,
 				s->min_nr_accesses, s->max_nr_accesses,
 				s->min_age_region, s->max_age_region,
-				s->action);
+				s->action, s->stat_count, s->stat_sz);
 		if (!rc)
 			return -ENOMEM;
 
