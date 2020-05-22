1eaea33efcd6230d848322b0a548dfdd43a2b59a
SeongJae Park <sjpark@amazon.de>
2020-05-22 09:52:05 +0200

    mm/damon: Implement region based sampling
    This commit implements DAMON's basic access check and region based
    sampling mechanisms.  This change would seems make no sense, mainly
    because it is only a part of the DAMON's logics.  Following two commits
    will make more sense.
    
    Basic Access Check
    ------------------
    
    DAMON basically reports what pages are how frequently accessed.  Note
    that the frequency is not an absolute number of accesses, but a relative
    frequency among the pages of the target workloads.
    
    Users can control the resolution of the reports by setting two time
    intervals, ``sampling interval`` and ``aggregation interval``.  In
    detail, DAMON checks access to each page per ``sampling interval``,
    aggregates the results (counts the number of the accesses to each page),
    and reports the aggregated results per ``aggregation interval``.  For
    the access check of each page, DAMON uses the Accessed bits of PTEs.
    
    This is thus similar to common periodic access checks based access
    tracking mechanisms, which overhead is increasing as the size of the
    target process grows.
    
    Region Based Sampling
    ---------------------
    
    To avoid the unbounded increase of the overhead, DAMON groups a number
    of adjacent pages that assumed to have same access frequencies into a
    region.  As long as the assumption (pages in a region have same access
    frequencies) is kept, only one page in the region is required to be
    checked.  Thus, for each ``sampling interval``, DAMON randomly picks one
    page in each region and clears its Accessed bit.  After one more
    ``sampling interval``, DAMON reads the Accessed bit of the page and
    increases the access frequency of the region if the bit has set
    meanwhile.  Therefore, the monitoring overhead is controllable by
    setting the number of regions.
    
    Nonetheless, this scheme cannot preserve the quality of the output if
    the assumption is not kept.  Following commit will introduce how we can
    make the guarantee with best effort.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -11,6 +11,8 @@
 #define _DAMON_H_
 
 #include <linux/random.h>
+#include <linux/mutex.h>
+#include <linux/time64.h>
 #include <linux/types.h>
 
 /**
@@ -44,11 +46,55 @@ struct damon_task {
 };
 
 /**
- * struct damon_ctx - Represents a context for each monitoring.
+ * struct damon_ctx - Represents a context for each monitoring.  This is the
+ * main interface that allows users to set the attributes and get the results
+ * of the monitoring.
+ *
+ * For each monitoring request (damon_start()), a kernel thread for the
+ * monitoring is created.  The pointer to the thread is stored in @kdamond.
+ *
+ * @sample_interval:		The time between access samplings.
+ * @aggr_interval:		The time between monitor results aggregations.
+ * @min_nr_regions:		The number of initial monitoring regions.
+ *
+ * For each @sample_interval, DAMON checks whether each region is accessed or
+ * not.  It aggregates and keeps the access information (number of accesses to
+ * each region) for @aggr_interval time.  All time intervals are in
+ * micro-seconds.
+ *
+ * @kdamond:		Kernel thread who does the monitoring.
+ * @kdamond_stop:	Notifies whether kdamond should stop.
+ * @kdamond_lock:	Mutex for the synchronizations with @kdamond.
+ *
+ * The monitoring thread sets @kdamond to NULL when it terminates.  Therefore,
+ * users can know whether the monitoring is ongoing or terminated by reading
+ * @kdamond.  Also, users can ask @kdamond to be terminated by writing non-zero
+ * to @kdamond_stop.  Reads and writes to @kdamond and @kdamond_stop from
+ * outside of the monitoring thread must be protected by @kdamond_lock.
+ *
+ * Note that the monitoring thread protects only @kdamond and @kdamond_stop via
+ * @kdamond_lock.  Accesses to other fields must be protected by themselves.
+ *
  * @tasks_list:		Head of monitring target tasks (&damon_task) list.
  */
 struct damon_ctx {
+	unsigned long sample_interval;
+	unsigned long aggr_interval;
+	unsigned long min_nr_regions;
+
+	struct timespec64 last_aggregation;
+
+	struct task_struct *kdamond;
+	bool kdamond_stop;
+	struct mutex kdamond_lock;
+
 	struct list_head tasks_list;	/* 'damon_task' objects */
 };
+
+int damon_set_pids(struct damon_ctx *ctx, int *pids, ssize_t nr_pids);
+int damon_set_attrs(struct damon_ctx *ctx, unsigned long sample_int,
+		unsigned long aggr_int, unsigned long min_nr_reg);
+int damon_start(struct damon_ctx *ctx);
+int damon_stop(struct damon_ctx *ctx);
 
 #endif
