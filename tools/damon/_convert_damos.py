7d6045352d56ff281cf86aa03f80f74f31b4fce7
SeongJae Park <sjpark@amazon.de>
2020-07-03 13:43:02 +0000

    (squash) tools/damon/_convert_damos: Replace null with min and max
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- tools/damon/_convert_damos.py
+++ tools/damon/_convert_damos.py
@@ -7,59 +7,71 @@ level input for the '<debugfs>/damon/schemes' file.  Below is an example of the
 schemes written in the human readable format:
 
     # format is:
-    # <min/max size> <min/max frequency (0-99)> <min/max age> <action>
+    # <min/max size> <min/max frequency (0-100)> <min/max age> <action>
     #
     # B/K/M/G/T for Bytes/KiB/MiB/GiB/TiB
     # us/ms/s/m/h/d for micro-seconds/milli-seconds/seconds/minutes/hours/days
-    # 'null' means zero for size and age.
+    # 'min/max' for possible min/max value.
 
     # if a region keeps a high access frequency for >=100ms, put the region on
     # the head of the LRU list (call madvise() with MADV_WILLNEED).
-    null    null    80      null    100ms   0s      willneed
+    min    max      80      max     100ms   max willneed
 
     # if a region keeps a low access frequency for >=200ms and <=one hour, put
     # the region on the tail of the LRU list (call madvise() with MADV_COLD).
-    0B      0B      10      20      200ms   1h cold
+    min     max     10      20      200ms   1h  cold
 
     # if a region keeps a very low access frequency for >=60 seconds, swap out
     # the region immediately (call madvise() with MADV_PAGEOUT).
-    0B      null    0       10      60s     0s pageout
+    min     max     0       10      60s     max pageout
 
     # if a region of a size >=2MiB keeps a very high access frequency for
     # >=100ms, let the region to use huge pages (call madvise() with
     # MADV_HUGEPAGE).
-    2M      null    90      99      100ms   0s hugepage
+    2M      max     90      100     100ms   max hugepage
 
     # If a regions of a size >=2MiB keeps small access frequency for >=100ms,
     # avoid the region using huge pages (call madvise() with MADV_NOHUGEPAGE).
-    2M      null    0       25      100ms   0s nohugepage
+    2M      max     0       25      100ms   max nohugepage
 """
 
 import argparse
+import platform
+
+uint_max = 2**32 - 1
+ulong_max = 2**64 - 1
+if platform.architecture()[0] != '64bit':
+    ulong_max = 2**32 - 1
 
 unit_to_bytes = {'B': 1, 'K': 1024, 'M': 1024 * 1024, 'G': 1024 * 1024 * 1024,
         'T': 1024 * 1024 * 1024 * 1024}
 
 def text_to_bytes(txt):
-    if txt == 'null':
+    if txt == 'min':
         return 0
+    if txt == 'max':
+        return ulong_max
+
     unit = txt[-1]
-    number = int(txt[:-1])
-    return number * unit_to_bytes[unit]
+    number = float(txt[:-1])
+    return int(number * unit_to_bytes[unit])
 
 unit_to_usecs = {'us': 1, 'ms': 1000, 's': 1000 * 1000, 'm': 60 * 1000 * 1000,
         'h': 60 * 60 * 1000 * 1000, 'd': 24 * 60 * 60 * 1000 * 1000}
 
-def text_to_us(txt):
-    if txt == 'null':
+def text_to_aggr_intervals(txt, aggr_interval):
+    if txt == 'min':
         return 0
+    if txt == 'max':
+        return uint_max
+
     unit = txt[-2:]
     if unit in ['us', 'ms']:
-        number = int(txt[:-2])
+        number = float(txt[:-2])
     else:
         unit = txt[-1]
-        number = int(txt[:-1])
-    return number * unit_to_usecs[unit]
+        number = float(txt[:-1])
+    return int(number * unit_to_usecs[unit]) / aggr_interval
 
 damos_action_to_int = {'DAMOS_WILLNEED': 0, 'DAMOS_COLD': 1,
         'DAMOS_PAGEOUT': 2, 'DAMOS_HUGEPAGE': 3, 'DAMOS_NOHUGEPAGE': 4,
@@ -69,9 +81,12 @@ def text_to_damos_action(txt):
     return damos_action_to_int['DAMOS_' + txt.upper()]
 
 def text_to_nr_accesses(txt, max_nr_accesses):
-    if txt == 'null':
+    if txt == 'min':
         return 0
-    return int(int(txt) * max_nr_accesses / 100)
+    if txt == 'max':
+        return uint_max
+
+    return int(float(txt) * max_nr_accesses / 100)
 
 def debugfs_scheme(line, sample_interval, aggr_interval):
     fields = line.split()
@@ -85,8 +100,8 @@ def debugfs_scheme(line, sample_interval, aggr_interval):
         max_sz = text_to_bytes(fields[1])
         min_nr_accesses = text_to_nr_accesses(fields[2], limit_nr_accesses)
         max_nr_accesses = text_to_nr_accesses(fields[3], limit_nr_accesses)
-        min_age = text_to_us(fields[4]) / aggr_interval
-        max_age = text_to_us(fields[5]) / aggr_interval
+        min_age = text_to_aggr_intervals(fields[4], aggr_interval)
+        max_age = text_to_aggr_intervals(fields[5], aggr_interval)
         action = text_to_damos_action(fields[6])
     except:
         print('wrong input field')
