fb81a3fe7efee104431ac9953f91f6d61027c890
SeongJae Park <sjpark@amazon.de>
2020-07-21 08:28:04 +0000

    mm/damon/debugfs: Support physical memory monitoring
    This commit makes the debugfs interface to support the physical memory
    monitoring, in addition to the virtual memory monitoring.
    
    Users can do the physical memory monitoring by writing a special
    keyword, 'paddr\n' to the 'pids' debugfs file.  Then, DAMON will check
    the special keyword and configure the callbacks of the monitoring
    context for the debugfs user for physical memory.  This will internally
    add one fake monitoring target process, which has pid as -1.
    
    Unlike the virtual memory monitoring, DAMON debugfs will not
    automatically set the monitoring target region.  Therefore, users should
    also set the monitoring target address region using the 'init_regions'
    debugfs file.  While doing this, the 'pid' in the input should be '-1'.
    
    Finally, the physical memory monitoring will not automatically
    terminated because it has fake monitoring target process.  The user
    should explicitly turn off the monitoring by writing 'off' to the
    'monitor_on' debugfs file.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- mm/damon.c
+++ mm/damon.c
@@ -2029,9 +2029,27 @@ static ssize_t debugfs_target_ids_write(struct file *file,
 		return PTR_ERR(kbuf);
 
 	nrs = kbuf;
-	if (!strncmp(kbuf, "pidfd ", 6)) {
-		received_pidfds = true;
-		nrs = &kbuf[6];
+	if (!strncmp(kbuf, "paddr\n", count)) {
+		/* Configure the context for physical memory monitoring */
+		ctx->init_target_regions = kdamond_init_phys_regions;
+		ctx->update_target_regions = kdamond_update_phys_regions;
+		ctx->prepare_access_checks = kdamond_prepare_phys_access_checks;
+		ctx->check_accesses = kdamond_check_phys_accesses;
+		ctx->target_valid = NULL;
+
+		/* Set the fake target task pid as -1 */
+		snprintf(kbuf, count, "-1    ");
+	} else {
+		/* Configure the context for virtual memory monitoring */
+		ctx->init_target_regions = kdamond_init_vm_regions;
+		ctx->update_target_regions = kdamond_update_vm_regions;
+		ctx->prepare_access_checks = kdamond_prepare_vm_access_checks;
+		ctx->check_accesses = kdamond_check_vm_accesses;
+		ctx->target_valid = kdamond_vm_target_valid;
+		if (!strncmp(kbuf, "pidfd ", 6)) {
+			received_pidfds = true;
+			nrs = &kbuf[6];
+		}
 	}
 
 	targets = str_to_target_ids(nrs, ret, &nr_targets);
