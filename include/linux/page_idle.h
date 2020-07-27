23358911bd9bfeab35d961be9b5e4ae0064dde9e
SeongJae Park <sjpark@amazon.de>
2020-07-27 10:26:47 +0000

    page_idle: Make dependent on DAMON, either
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- include/linux/page_idle.h
+++ include/linux/page_idle.h
@@ -6,7 +6,7 @@
 #include <linux/page-flags.h>
 #include <linux/page_ext.h>
 
-#ifdef CONFIG_IDLE_PAGE_TRACKING
+#if defined(CONFIG_IDLE_PAGE_TRACKING) || defined(CONFIG_DAMON)
 
 #ifdef CONFIG_64BIT
 static inline bool page_is_young(struct page *page)
@@ -106,7 +106,7 @@ static inline void clear_page_idle(struct page *page)
 }
 #endif /* CONFIG_64BIT */
 
-#else /* !CONFIG_IDLE_PAGE_TRACKING */
+#else /* !CONFIG_IDLE_PAGE_TRACKING && !CONFIG_DAMON */
 
 static inline bool page_is_young(struct page *page)
 {
@@ -135,6 +135,6 @@ static inline void clear_page_idle(struct page *page)
 {
 }
 
-#endif /* CONFIG_IDLE_PAGE_TRACKING */
+#endif /* CONFIG_IDLE_PAGE_TRACKING && CONFIG_DAMON */
 
 #endif /* _LINUX_MM_PAGE_IDLE_H */
