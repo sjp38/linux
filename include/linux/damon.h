cee2bd808a86d272ec22264f2cf11cfd835f5567
SeongJae Park <sjpark@amazon.de>
2020-10-01 16:26:07 +0000

    mm/damon: Implement a debugfs-based user space interface
    This commit implements a debugfs interface for DAMON.  It works for the
    virtual address spaces monitoring.
    
    DAMON exports four files, ``attrs``, ``target_ids``, ``record``, and
    ``monitor_on`` under its debugfs directory, ``<debugfs>/damon/``.
    
    Attributes
    ----------
    
    Users can read and write the ``sampling interval``, ``aggregation
    interval``, ``regions update interval``, and min/max number of
    monitoring target regions by reading from and writing to the ``attrs``
    file.  For example, below commands set those values to 5 ms, 100 ms,
    1,000 ms, 10, 1000 and check it again::
    
        # cd <debugfs>/damon
        # echo 5000 100000 1000000 10 1000 > attrs
        # cat attrs
        5000 100000 1000000 10 1000
    
    Target IDs
    ----------
    
    Some types of address spaces supports multiple monitoring target.  For
    example, the virtual memory address spaces monitoring can have multiple
    processes as the monitoring targets.  Users can set the targets by
    writing relevant id values of the targets to, and get the ids of the
    current targets by reading from the ``target_ids`` file.  In case of the
    virtual address spaces monitoring, the values should be pids of the
    monitoring target processes.  For example, below commands set processes
    having pids 42 and 4242 as the monitoring targets and check it again::
    
        # cd <debugfs>/damon
        # echo 42 4242 > target_ids
        # cat target_ids
        42 4242
    
    Note that setting the target ids doesn't start the monitoring.
    
    Turning On/Off
    --------------
    
    Setting the files as described above doesn't incur effect unless you
    explicitly start the monitoring.  You can start, stop, and check the
    current status of the monitoring by writing to and reading from the
    ``monitor_on`` file.  Writing ``on`` to the file starts the monitoring
    of the targets with the attributes.  Writing ``off`` to the file stops
    those.  DAMON also stops if every targets are invalidated (in case of
    the virtual memory monitoring, target processes are invalidated when
    terminated).  Below example commands turn on, off, and check the status
    of DAMON::
    
        # cd <debugfs>/damon
        # echo on > monitor_on
        # echo off > monitor_on
        # cat monitor_on
        off
    
    Please note that you cannot write to the above-mentioned debugfs files
    while the monitoring is turned on.  If you write to the files while
    DAMON is running, an error code such as ``-EBUSY`` will be returned.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>
    Reviewed-by: Leonard Foerster <foersleo@amazon.de>

--- include/linux/damon.h
+++ include/linux/damon.h
@@ -187,11 +187,14 @@ void damon_free_target(struct damon_target *t);
 void damon_destroy_target(struct damon_target *t);
 unsigned int damon_nr_regions(struct damon_target *t);
 
+struct damon_ctx *damon_new_ctx(void);
+void damon_destroy_ctx(struct damon_ctx *ctx);
 int damon_set_targets(struct damon_ctx *ctx,
 		unsigned long *ids, ssize_t nr_ids);
 int damon_set_attrs(struct damon_ctx *ctx, unsigned long sample_int,
 		unsigned long aggr_int, unsigned long regions_update_int,
 		unsigned long min_nr_reg, unsigned long max_nr_reg);
+int damon_nr_running_ctxs(void);
 int damon_start(struct damon_ctx **ctxs, int nr_ctxs);
 int damon_stop(struct damon_ctx **ctxs, int nr_ctxs);
 
