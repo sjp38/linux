3e22b7296fd48afee85c95eaf445c0abb8702ef5
SeongJae Park <sjpark@amazon.de>
2020-10-06 14:05:52 +0000

    for_damon_hack: Add files for DAMON hacks
    This commit adds text files and scripts for easier DAMON hacks and
    patches preparation.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ for_damon_hack/add_git_remotes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git remote add akpm-mm https://github.com/hnaz/linux-mm
+git remote add gh.damon        git@github.com:damonitor/linux.git
+git remote add gh.public       git@github.com:sjp38/linux.git
+git remote add linus   git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
+git remote add sj.korg git@gitolite.kernel.org:pub/scm/linux/kernel/git/sj/linux
+git remote add stable  git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git
