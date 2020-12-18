a4307b352184fab22b0ac827f3aa5586befe9690
SeongJae Park <sjpark@amazon.de>
2020-12-18 15:50:06 +0000

    tools/perf: Integrate DAMON in perf
    Though there is a simple debugfs interface for DAMON and even a
    dedicated user space tool, integrating the interface in perf will make
    best user experiences.  For the reason, this commit adds perf scripts
    for DAMON.  After this commit, users can record the data access
    monitoring results and read in human readable form with:
    
        $ sudo perf script record damon
        $ sudo perf script report damon
    
    or simply,
    
        $ sudo perf script damon raw
    
    Nevertheless, above commands do not start the monitoring by themselves,
    so users should turn it on background on their own.  To make it easy,
    this commit also adds a convenient version of 'perf script record
    damon', which executes a command, turns on DAMON for the process, and
    records DAMON trace events.  For example, it can be used as:
    
        $ sudo ~/libexec/perf-core/scripts/python/bin/damon-record.sh <cmd>
        $ sudo perf script report damon
    
    Currently, the report command supports only a raw format report.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

---
+++ tools/perf/scripts/python/damon.py
@@ -0,0 +1,112 @@
+# SPDX-License-Identifier: GPL-2.0
+#
+# Author: SeongJae Park <sjpark@amazon.de>
+
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+
+sys.path.append(os.environ['PERF_EXEC_PATH'] + \
+	'/scripts/python/Perf-Trace-Util/lib/Perf/Trace')
+
+from perf_trace_context import *
+from Core import *
+
+class Region:
+	start = None
+	end = None
+	nr_accesses = None
+
+	def __init__(self, start, end, nr_accesses):
+		self.start = start
+		self.end = end
+		self.nr_accesses = nr_accesses
+
+class Snapshot:
+	monitored_time = None
+	target_id = None
+	regions = None
+
+	def __init__(self, monitored_time, target_id):
+		self.monitored_time = monitored_time
+		self.target_id = target_id
+		self.regions = []
+
+class Record:
+	start_time = None
+	snapshots = None
+
+	def __init__(self, start_time):
+		self.start_time = start_time
+		self.snapshots = []
+
+def trace_begin():
+	pass
+
+def trace_end():
+	print_record_raw(record, args.sz_bytes)
+
+args = None
+record = None
+nr_read_regions = 0
+def damon__damon_aggregated(event_name, context, common_cpu,
+		common_secs, common_nsecs, common_pid, common_comm,
+		common_callchain, target_id, nr_regions, start, end,
+		nr_accesses, perf_sample_dict):
+	global record
+	global nr_read_regions
+
+	time = common_secs * 1000000000 + common_nsecs
+	if not record:
+		record = Record(time)
+
+	if nr_read_regions == 0:
+		snapshot = Snapshot(time, target_id)
+		record.snapshots.append(snapshot)
+
+	snapshot = record.snapshots[-1]
+	snapshot.regions.append(Region(start, end, nr_accesses))
+
+	nr_read_regions += 1
+	if nr_read_regions == nr_regions:
+		nr_read_regions = 0
+
+def format_sz(number, sz_bytes):
+	if sz_bytes:
+		return '%d' % number
+
+	if number > 1<<40:
+		return '%.3f TiB' % (number / (1<<40))
+	if number > 1<<30:
+		return '%.3f GiB' % (number / (1<<30))
+	if number > 1<<20:
+		return '%.3f MiB' % (number / (1<<20))
+	if number > 1<<10:
+		return '%.3f KiB' % (number / (1<<10))
+	return '%d B' % number
+
+def print_record_raw(record, sz_bytes):
+	print('start_time:', record.start_time)
+	for snapshot in record.snapshots:
+		print('relative_time:',
+				snapshot.monitored_time - record.start_time)
+		print('target_id:', snapshot.target_id)
+		print('nr_regions:', len(snapshot.regions))
+		for region in snapshot.regions:
+			print('%x-%x (%s): %d' % (region.start, region.end,
+				format_sz(region.end - region.start, sz_bytes),
+				region.nr_accesses))
+		print()
+
+def main():
+	global args
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--sz-bytes', action='store_true',
+			help='report size in bytes')
+	args = parser.parse_args()
+
+if __name__ == '__main__':
+	main()
