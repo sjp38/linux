0498abe7779db68c8f4ced205821467f14e31514
SeongJae Park <sjpark@amazon.de>
2021-03-30 07:30:39 +0000

    (squash) mm/damon/vaddr: Handle special mappings
    A process could have special virtual memory mappings that have no
    corresponding 'struct page'.  In that case, DAMON primitives for virtual
    address spaces could access bad address that assumed to have a 'struct
    page' and crashes as a result.  This commit fixes the issue by making a
    wrapper for getting 'struct page' and using it.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>
    Reported-by: Guoju Fang <guoju.fgj@alibaba-inc.com>

--- mm/damon/vaddr.c
+++ mm/damon/vaddr.c
@@ -364,11 +364,35 @@ void damon_va_update(struct damon_ctx *ctx)
 	}
 }
 
+/*
+ * Get an online page for a pfn if it's in the LRU list.  Otherwise, returns
+ * NULL.
+ *
+ * The body of this function is stollen from the 'page_idle_get_page()'.  We
+ * steal rather than reuse it because the code is quite simple.
+ */
+static struct page *damon_get_page(unsigned long pfn)
+{
+	struct page *page = pfn_to_online_page(pfn);
+
+	if (!page || !PageLRU(page) || !get_page_unless_zero(page))
+		return NULL;
+
+	if (unlikely(!PageLRU(page))) {
+		put_page(page);
+		page = NULL;
+	}
+	return page;
+}
+
 static void damon_ptep_mkold(pte_t *pte, struct mm_struct *mm,
 			     unsigned long addr)
 {
 	bool referenced = false;
-	struct page *page = pte_page(*pte);
+	struct page *page = damon_get_page(pte_pfn(*pte));
+
+	if (!page)
+		return;
 
 	if (pte_young(*pte)) {
 		referenced = true;
@@ -384,6 +408,7 @@ static void damon_ptep_mkold(pte_t *pte, struct mm_struct *mm,
 		set_page_young(page);
 
 	set_page_idle(page);
+	put_page(page);
 }
 
 static void damon_pmdp_mkold(pmd_t *pmd, struct mm_struct *mm,
@@ -391,7 +416,10 @@ static void damon_pmdp_mkold(pmd_t *pmd, struct mm_struct *mm,
 {
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	bool referenced = false;
-	struct page *page = pmd_page(*pmd);
+	struct page *page = damon_get_page(pmd_pfn(*pmd));
+
+	if (!page)
+		return;
 
 	if (pmd_young(*pmd)) {
 		referenced = true;
@@ -408,6 +436,7 @@ static void damon_pmdp_mkold(pmd_t *pmd, struct mm_struct *mm,
 		set_page_young(page);
 
 	set_page_idle(page);
+	put_page(page);
 #endif /* CONFIG_TRANSPARENT_HUGEPAGE */
 }
 
@@ -463,6 +492,7 @@ static bool damon_va_young(struct mm_struct *mm, unsigned long addr,
 	pte_t *pte = NULL;
 	pmd_t *pmd = NULL;
 	spinlock_t *ptl;
+	struct page *page;
 	bool young = false;
 
 	if (follow_invalidate_pte(mm, addr, NULL, &pte, &pmd, &ptl))
@@ -470,17 +500,24 @@ static bool damon_va_young(struct mm_struct *mm, unsigned long addr,
 
 	*page_sz = PAGE_SIZE;
 	if (pte) {
-		if (pte_young(*pte) || !page_is_idle(pte_page(*pte)) ||
-				mmu_notifier_test_young(mm, addr))
+		page = damon_get_page(pte_pfn(*pte));
+		if (page && (pte_young(*pte) || !page_is_idle(page) ||
+					mmu_notifier_test_young(mm, addr)))
 			young = true;
+		if (page)
+			put_page(page);
 		pte_unmap_unlock(pte, ptl);
 		return young;
 	}
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
-	if (pmd_young(*pmd) || !page_is_idle(pmd_page(*pmd)) ||
-			mmu_notifier_test_young(mm, addr))
+	page = damon_get_page(pmd_pfn(*pmd));
+	if (page && (pmd_young(*pmd) || !page_is_idle(page) ||
+			mmu_notifier_test_young(mm, addr)))
 		young = true;
+	if (page)
+		put_page(page);
+
 	spin_unlock(ptl);
 	*page_sz = ((1UL) << HPAGE_PMD_SHIFT);
 #endif	/* CONFIG_TRANSPARENT_HUGEPAGE */
