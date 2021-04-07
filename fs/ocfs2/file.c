61851a47a628ea475cd609de16bfcf096c656dc4
Wengang Wang <wen.gang.wang@oracle.com>
2021-04-07 02:00:18 -0400

    ocfs2-fix-deadlock-between-setattr-and-dio_end_io_write-v2
    remove the "had_alloc_lock" as suggested
    
    Link: https://lkml.kernel.org/r/20210402171344.1605-1-wen.gang.wang@oracle.com
    Cc: stable@vger.kernel.org
    Signed-off-by: Wengang Wang <wen.gang.wang@oracle.com>
    Cc: Mark Fasheh <mark@fasheh.com>
    Cc: Joel Becker <jlbec@evilplan.org>
    Cc: Junxiao Bi <junxiao.bi@oracle.com>
    Cc: Joseph Qi <jiangqi903@gmail.com>
    Cc: Changwei Ge <gechangwei@live.cn>
    Cc: Gang He <ghe@suse.com>
    Cc: Jun Piao <piaojun@huawei.com>
    
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- fs/ocfs2/file.c
+++ fs/ocfs2/file.c
@@ -1124,7 +1124,7 @@ int ocfs2_setattr(struct user_namespace *mnt_userns, struct dentry *dentry,
 	handle_t *handle = NULL;
 	struct dquot *transfer_to[MAXQUOTAS] = { };
 	int qtype;
-	int had_lock, had_alloc_lock = 0;
+	int had_lock;
 	struct ocfs2_lock_holder oh;
 
 	trace_ocfs2_setattr(inode, dentry,
@@ -1246,27 +1246,23 @@ int ocfs2_setattr(struct user_namespace *mnt_userns, struct dentry *dentry,
 			}
 		}
 		down_write(&OCFS2_I(inode)->ip_alloc_sem);
-		had_alloc_lock = 1;
-
 		handle = ocfs2_start_trans(osb, OCFS2_INODE_UPDATE_CREDITS +
 					   2 * ocfs2_quota_trans_credits(sb));
 		if (IS_ERR(handle)) {
 			status = PTR_ERR(handle);
 			mlog_errno(status);
-			goto bail_unlock;
+			goto bail_unlock_alloc;
 		}
 		status = __dquot_transfer(inode, transfer_to);
 		if (status < 0)
 			goto bail_commit;
 	} else {
 		down_write(&OCFS2_I(inode)->ip_alloc_sem);
-		had_alloc_lock = 1;
-
 		handle = ocfs2_start_trans(osb, OCFS2_INODE_UPDATE_CREDITS);
 		if (IS_ERR(handle)) {
 			status = PTR_ERR(handle);
 			mlog_errno(status);
-			goto bail_unlock;
+			goto bail_unlock_alloc;
 		}
 	}
 
@@ -1279,10 +1275,9 @@ int ocfs2_setattr(struct user_namespace *mnt_userns, struct dentry *dentry,
 
 bail_commit:
 	ocfs2_commit_trans(osb, handle);
+bail_unlock_alloc:
+	up_write(&OCFS2_I(inode)->ip_alloc_sem);
 bail_unlock:
-	if (had_alloc_lock)
-		up_write(&OCFS2_I(inode)->ip_alloc_sem);
-
 	if (status && inode_locked) {
 		ocfs2_inode_unlock_tracker(inode, 1, &oh, had_lock);
 		inode_locked = 0;
