953737bfcdcf403a590c0d40202a9df2238019cf
SeongJae Park <sjpark@amazon.de>
2021-05-26 14:39:02 +0000

    mm/damon/schemes: Activate schemes based on a watermarks mechanism
    DAMON-based operation schemes need to be manually turned on and off.  In
    some use cases, however, the condition for turning a scheme on and off
    would depend on the system's situation.  For example, schemes for
    proactive pages reclamation would need to be turned on when some memory
    pressure is detected, and turned off when the system has enough free
    memory.
    
    For easier control of schemes activation based on the system situation,
    this commit introduces a watermarks-based mechanism.  The client can
    describe the watermark metric (e.g., amount of free memory in the
    system), watermark check interval, and three watermarks, namely high,
    mid, and low.  If the scheme is deactivated, it only gets the metric and
    compare that to the three watermarks for every check interval.  If the
    metric is higher than the high watermark, the scheme is deactivated.  If
    the metric is between the mid watermark and the low watermark, the
    scheme is activated.  If the metric is lower than the low watermark, the
    scheme is deactivated again.  This is to allow users fall back to
    traditional page-granularity mechanisms.
    
    Signed-off-by: SeongJae Park <sjpark@amazon.de>

--- mm/damon/dbgfs.c
+++ mm/damon/dbgfs.c
@@ -315,6 +315,9 @@ static struct damos **str_to_schemes(const char *str, ssize_t len,
 	*nr_schemes = 0;
 	while (pos < len && *nr_schemes < max_nr_schemes) {
 		struct damos_speed_limit limit = {};
+		struct damos_watermarks wmarks = {
+			.metric = DAMOS_WMARK_NONE,
+		};
 
 		ret = sscanf(&str[pos],
 				"%lu %lu %u %u %u %u %u %lu %lu %u %u %u%n",
@@ -332,7 +335,7 @@ static struct damos **str_to_schemes(const char *str, ssize_t len,
 
 		pos += parsed;
 		scheme = damon_new_scheme(min_sz, max_sz, min_nr_a, max_nr_a,
-				min_age, max_age, action, &limit);
+				min_age, max_age, action, &limit, &wmarks);
 		if (!scheme)
 			goto fail;
 
