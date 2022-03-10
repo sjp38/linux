8b588d5fca672d960b31361b9cdd9ee3b907bd9a
SeongJae Park <sj@kernel.org>
2022-03-10 10:52:46 +0000

    selftests/damon/dbgfs_target_ids_pid_leak: Remove slabtop dependency
    DAMON selftest 'dbgfs_target_ids_pid_leak' depends on slabtop, though
    the information can also retrieved from '/proc/slabinfo'.  This commit
    makes the test to use the proc file directly and remove the unnecessary
    dependency to slabtop.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- tools/testing/selftests/damon/dbgfs_target_ids_pid_leak.sh
+++ tools/testing/selftests/damon/dbgfs_target_ids_pid_leak.sh
@@ -3,19 +3,11 @@
 
 ksft_skip=4
 
-if ! which slabtop > /dev/null
-then
-	echo "slabtop not found"
-	exit $ksft_skip
-fi
-
-before=$(script -q -c "stty rows 999; sudo slabtop -o" /dev/null | grep pid | \
-	awk '{print $2}')
+before=$(grep "^pid " /proc/slabinfo | awk '{print $2}')
 
 timeout 1 ./dbgfs_target_ids_pid_leak
 
-after=$(script -q -c "stty rows 999; sudo slabtop -o" /dev/null | grep pid | \
-	awk '{print $2}')
+after=$(grep "^pid " /proc/slabinfo | awk '{print $2}')
 
 echo "number of active pid slabs: $before -> $after"
 if [ $after -gt $before ]
