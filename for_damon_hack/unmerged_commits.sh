072943a96a8b40d534ad4488775af5a350c9a3b6
SeongJae Park <sj@kernel.org>
2022-04-05 10:33:18 +0000

    for_damon_hack/unmerged_commits: Add an option for more human readable output
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- for_damon_hack/unmerged_commits.sh
+++ for_damon_hack/unmerged_commits.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
+if [ $# -ne 2 ] && [ $# -ne 3 ]
 then
-	echo "Usage: $0 <dev commits> <maintainer tree commits>"
+	echo "Usage: $0 <dev commits> <maintainer tree commits> [--human_readable]"
 	exit 1
 fi
 
 dev_commits_range=$1
 maintainer_commits_range=$2
+
+if [ $# -eq 3 ] && [ "$3" = "--human_readable" ]
+then
+	human_readable="true"
+else
+	human_readable="false"
+fi
 
 dev_commits=$(git log "$dev_commits_range" --pretty=%H --reverse)
 maintainer_commits=$(git log "$maintainer_commits_range" --pretty=%H --reverse)
@@ -23,5 +30,10 @@ do
 		continue
 	fi
 
-	echo "$dev_commit"
+	if [ "$human_readable" = "true" ]
+	then
+		echo "$dev_commit ($dev_subject)"
+	else
+		echo "$dev_commit"
+	fi
 done
