18152d961ff96d019e56a1779501abe7ccfab700
SeongJae Park <sj@kernel.org>
2023-06-30 21:51:47 +0000

    Revert "include/trace/events/damon: print out moving_accesses_bp of region"
    This reverts commit 8700c208b29354e96ea5f91f2d08a3094562f2c3.
    
    moving average is for sampling interval access tracking.  Printing it
    for damon_aggregated event makes no sense.  Revert.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- include/trace/events/damon.h
+++ include/trace/events/damon.h
@@ -34,11 +34,10 @@ TRACE_EVENT(damon_aggregated,
 		__entry->age = r->age;
 	),
 
-	TP_printk("target_id=%lu nr_regions=%u %lu-%lu: %u %u %u",
+	TP_printk("target_id=%lu nr_regions=%u %lu-%lu: %u %u",
 			__entry->target_id, __entry->nr_regions,
 			__entry->start, __entry->end,
-			__entry->nr_accesses, __entry->age,
-			__entry->moving_accesses_bp)
+			__entry->nr_accesses, __entry->age)
 );
 
 #endif /* _TRACE_DAMON_H */
