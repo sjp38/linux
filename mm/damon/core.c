7461d9e804f0bd55d93f4b2b1e5c41d83b94bd62
SeongJae Park <sj@kernel.org>
2023-11-11 19:36:03 +0000

    mm/damon/core: add debugging-purpose log of tuned esz
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- mm/damon/core.c
+++ mm/damon/core.c
@@ -1598,6 +1598,8 @@ static void damos_set_effective_quota(struct damos_quota *quota)
 		esz = quota->sz;
 
 	quota->esz = esz;
+
+	pr_info("esz %lu\n", esz);
 }
 
 static void damos_adjust_quota(struct damon_ctx *c, struct damos *s)
