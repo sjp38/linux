cac49d339abae213adcd4fa23294c59c90d8db06
SeongJae Park <sj@kernel.org>
2024-05-04 15:23:05 -0700

    mm/damon: implement DAMOS actions for access-aware contiguous memory allocation
    Implement two DAMOS actions, namely DAMOS_ALLOC and DAMOS_FREE.  As the
    name says, the actions allocate/de-allocate given DAMOS target memory in
    user-defined base granularity.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- mm/damon/sysfs-schemes.c
+++ mm/damon/sysfs-schemes.c
@@ -1566,6 +1566,10 @@ static const char * const damon_sysfs_damos_action_strs[] = {
 	"lru_deprio",
 	"migrate_hot",
 	"migrate_cold",
+#ifdef CONFIG_ACMA
+	"damos_alloc",
+	"damos_free",
+#endif
 	"stat",
 };
 
