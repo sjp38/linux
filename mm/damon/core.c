a5a07e4677a1ac80b42cc087ae6ba8ecab6a763b
SeongJae Park <sj@kernel.org>
2024-11-07 17:45:54 -0800

    mm/damon: remove ->before_start of damon_callback
    The function pointer field was added to be used as a place to do some
    initialization works just before DAMON starts working.  However, nobody
    is using it now.  Remove it.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- mm/damon/core.c
+++ mm/damon/core.c
@@ -2366,8 +2366,6 @@ static int kdamond_fn(void *data)
 
 	if (ctx->ops.init)
 		ctx->ops.init(ctx);
-	if (ctx->callback.before_start && ctx->callback.before_start(ctx))
-		goto done;
 	ctx->regions_score_histogram = kmalloc_array(DAMOS_MAX_SCORE + 1,
 			sizeof(*ctx->regions_score_histogram), GFP_KERNEL);
 	if (!ctx->regions_score_histogram)
