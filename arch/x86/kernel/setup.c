201e73e42d5a164945f9c726d528156c32d336b4
Alexander Graf <graf@amazon.com>
2025-02-06 15:27:52 +0200

    x86: add KHO support
    We now have all bits in place to support KHO kexecs.  This patch adds
    awareness of KHO in the kexec file as well as boot path for x86 and adds
    the respective kconfig option to the architecture so that it can use KHO
    successfully.
    
    In addition, it enlightens it decompression code with KHO so that its
    KASLR location finder only considers memory regions that are not already
    occupied by KHO memory.
    
    Link: https://lkml.kernel.org/r/20250206132754.2596694-13-rppt@kernel.org
    Signed-off-by: Alexander Graf <graf@amazon.com>
    Co-developed-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Anthony Yznaga <anthony.yznaga@oracle.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Ashish Kalra <ashish.kalra@amd.com>
    Cc: Ben Herrenschmidt <benh@kernel.crashing.org>
    Cc: Borislav Betkov <bp@alien8.de>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Cc: Eric Biederman <ebiederm@xmission.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: James Gowans <jgowans@amazon.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Krzysztof Kozlowski <krzk@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Pratyush Yadav <ptyadav@amazon.de>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: Rob Herring <robh@kernel.org>
    Cc: Saravana Kannan <saravanak@google.com>
    Cc: Stanislav Kinsburskii <skinsburskii@linux.microsoft.com>
    Cc: Steven Rostedt (VMware) <rostedt@goodmis.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Usama Arif <usama.arif@bytedance.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -385,6 +385,28 @@ int __init ima_get_kexec_buffer(void **addr, size_t *size)
 }
 #endif
 
+static void __init add_kho(u64 phys_addr, u32 data_len)
+{
+#ifdef CONFIG_KEXEC_HANDOVER
+	struct kho_data *kho;
+	u64 addr = phys_addr + sizeof(struct setup_data);
+	u64 size = data_len - sizeof(struct setup_data);
+
+	kho = early_memremap(addr, size);
+	if (!kho) {
+		pr_warn("setup: failed to memremap kho data (0x%llx, 0x%llx)\n",
+			addr, size);
+		return;
+	}
+
+	kho_populate(kho->dt_addr, kho->scratch_addr, kho->scratch_size);
+
+	early_memunmap(kho, size);
+#else
+	pr_warn("Passed KHO data, but CONFIG_KEXEC_HANDOVER not set. Ignoring.\n");
+#endif
+}
+
 static void __init parse_setup_data(void)
 {
 	struct setup_data *data;
@@ -412,6 +434,9 @@ static void __init parse_setup_data(void)
 			break;
 		case SETUP_IMA:
 			add_early_ima_buffer(pa_data);
+			break;
+		case SETUP_KEXEC_KHO:
+			add_kho(pa_data, data_len);
 			break;
 		case SETUP_RNG_SEED:
 			data = early_memremap(pa_data, data_len);
