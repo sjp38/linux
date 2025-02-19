602176610471f707c5c191f99fcec6ce181310a4
Ma Wupeng <mawupeng1@huawei.com>
2025-02-19 14:06:52 +0800

    mm-memory-failure-update-ttu-flag-inside-unmap_poisoned_folio-v3
    unmap_poisoned_folio(): remove shadowed local `mapping', per Miaohe
    
    Link: https://lkml.kernel.org/r/20250219060653.3849083-1-mawupeng1@huawei.com
    Fixes: 6da6b1d4a7df ("mm/hwpoison: convert TTU_IGNORE_HWPOISON to TTU_HWPOISON")
    Suggested-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Ma Wupeng <mawupeng1@huawei.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Acked-by: Miaohe Lin <linmiaohe@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -1585,8 +1585,6 @@ int unmap_poisoned_folio(struct folio *folio, unsigned long pfn, bool must_kill)
 	}
 
 	if (folio_test_hugetlb(folio) && !folio_test_anon(folio)) {
-		struct address_space *mapping;
-
 		/*
 		 * For hugetlb folios in shared mappings, try_to_unmap
 		 * could potentially call huge_pmd_unshare.  Because of
