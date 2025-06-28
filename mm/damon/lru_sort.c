3e26839768de2be2850ba89365eed13331188ea7
SeongJae Park <sj@kernel.org>
2025-06-28 13:59:20 -0700

    mm/damon/lru_sort: use damon_initialized()
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- mm/damon/lru_sort.c
+++ mm/damon/lru_sort.c
@@ -300,7 +300,7 @@ static int damon_lru_sort_enabled_store(const char *val,
 		return 0;
 
 	/* Called before init function.  The function will handle this. */
-	if (!ctx)
+	if (!damon_initialized())
 		goto set_param_out;
 
 	err = damon_lru_sort_turn(enable);
@@ -323,8 +323,13 @@ MODULE_PARM_DESC(enabled,
 
 static int __init damon_lru_sort_init(void)
 {
-	int err = damon_modules_new_paddr_ctx_target(&ctx, &target);
+	int err;
 
+	if (!damon_initialized()) {
+		err = -EINVAL;
+		goto out;
+	}
+	err = damon_modules_new_paddr_ctx_target(&ctx, &target);
 	if (err)
 		goto out;
 
