564790ee61f2e936c8df3958729d76724c6c2f19
SeongJae Park <sj@kernel.org>
2025-06-28 13:58:31 -0700

    mm/damon/reclaim: use damon_initialized()
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- mm/damon/reclaim.c
+++ mm/damon/reclaim.c
@@ -275,7 +275,7 @@ static int damon_reclaim_enabled_store(const char *val,
 		return 0;
 
 	/* Called before init function.  The function will handle this. */
-	if (!ctx)
+	if (!damon_initialized())
 		goto set_param_out;
 
 	err = damon_reclaim_turn(enable);
@@ -330,6 +330,8 @@ static int __init damon_reclaim_init(void)
 
 	if (err)
 		return err;
+	if (!damon_initialized())
+		return -ENOMEM;
 
 	ctx->callback.after_wmarks_check = damon_reclaim_after_wmarks_check;
 	ctx->callback.after_aggregation = damon_reclaim_after_aggregation;
