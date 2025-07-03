970f13b7d3d6bc503a1c076c6f6abff0de67ad62
Yunjeong Mun <yunjeong.mun@sk.com>
2025-07-03 16:44:22 +0900

    samples/damon: support automatic node address detection
    This patch adds a new knob `detect_node_addresses`, which determines
    whether the physical address range is set manually using the existing
    knobs or automatically by the mtier module. When `detect_node_addresses`
    set to 'Y', mtier automatically converts node0 and node1 to their
    physical addresses. If set to 'N', it uses the existing
    'node#_start_addr' and 'node#_end_addr' to define regions as before.
    
    Suggested-by: Honggyu Kim <honggyu.kim@sk.com>
    Signed-off-by: Yunjeong Mun <yunjeong.mun@sk.com>
    Link: https://patch.msgid.link/20250703074423.1771-1-yunjeong.mun@sk.com
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- samples/damon/mtier.c
+++ samples/damon/mtier.c
@@ -42,7 +42,33 @@ static bool enable __read_mostly;
 module_param_cb(enable, &enable_param_ops, &enable, 0600);
 MODULE_PARM_DESC(enable, "Enable of disable DAMON_SAMPLE_MTIER");
 
+static bool detect_node_addresses __read_mostly;
+module_param(detect_node_addresses, bool, 0600);
+
 static struct damon_ctx *ctxs[2];
+
+struct region_range {
+	phys_addr_t start;
+	phys_addr_t end;
+};
+
+static int nid_to_phys(int target_node, struct region_range *range)
+{
+
+	if (!node_online(target_node)) {
+		pr_err("NUMA node %d is not online\n", target_node);
+		return -EINVAL;
+	}
+
+	/* TODO: Do we need to support more accurate region range?  */
+	unsigned long start_pfn = node_start_pfn(target_node);
+	unsigned long end_pfn   = node_end_pfn(target_node);
+
+	range->start = PFN_PHYS(start_pfn);
+	range->end  = PFN_PHYS(end_pfn);
+
+	return 0;
+}
 
 static struct damon_ctx *damon_sample_mtier_build_ctx(bool promote)
 {
@@ -53,6 +79,8 @@ static struct damon_ctx *damon_sample_mtier_build_ctx(bool promote)
 	struct damos *scheme;
 	struct damos_quota_goal *quota_goal;
 	struct damos_filter *filter;
+	struct region_range addr;
+	int ret;
 
 	ctx = damon_new_ctx();
 	if (!ctx)
@@ -82,9 +110,17 @@ static struct damon_ctx *damon_sample_mtier_build_ctx(bool promote)
 	if (!target)
 		goto free_out;
 	damon_add_target(ctx, target);
-	region = damon_new_region(
-			promote ? node1_start_addr : node0_start_addr,
-			promote ? node1_end_addr : node0_end_addr);
+
+	if (detect_node_addresses) {
+		ret = promote ? nid_to_phys(1, &addr) : nid_to_phys(0, &addr);
+		if (ret)
+			goto free_out;
+	} else {
+		addr.start = promote ? node1_start_addr : node0_start_addr;
+		addr.end = promote ? node1_end_addr : node0_end_addr;
+	}
+
+	region = damon_new_region(addr.start, addr.end);
 	if (!region)
 		goto free_out;
 	damon_add_region(region, target);
