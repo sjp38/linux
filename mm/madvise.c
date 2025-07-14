d4b7b811dac6266e99cf0782f3ff9d523b3da0ee
Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
2025-07-14 15:29:59 +0100

    mm/madvise: correct comment
    We only either update vm_flags or anon vma name, not a combination of the
    two.  Per David.
    
    Link: https://lkml.kernel.org/r/77f45b2e-a748-4635-9381-a5051091087f@lucifer.local
    Signed-off-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Liam R. Howlett <Liam.Howlett@oracle.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/madvise.c
+++ mm/madvise.c
@@ -144,8 +144,8 @@ static int replace_anon_vma_name(struct vm_area_struct *vma,
 }
 #endif /* CONFIG_ANON_VMA_NAME */
 /*
- * Update the vm_flags and/or anon_name on region of a vma, splitting it or
- * merging it as necessary. Must be called with mmap_lock held for writing.
+ * Update the vm_flags or anon_name on region of a vma, splitting it or merging
+ * it as necessary. Must be called with mmap_lock held for writing.
  */
 static int madvise_update_vma(vm_flags_t new_flags,
 		struct madvise_behavior *madv_behavior)
