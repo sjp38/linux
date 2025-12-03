016b108925a117ea139296375ab7ef80048295b0
SeongJae Park <sj@kernel.org>
2025-12-02 22:32:56 -0800

    mm/damon/core: report access-generated thread id of the fault event
    Page fault event reporting function can know which thread is making the
    access.  Add the information to the report.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- mm/damon/core.c
+++ mm/damon/core.c
@@ -1741,6 +1741,7 @@ void damon_report_page_fault(struct vm_fault *vmf, bool huge_pmd)
 	struct damon_access_report access_report = {
 		.size = 1,	/* todo: set appripriately */
 		.cpu = smp_processor_id(),
+		.tid = task_pid_vnr(current),
 	};
 
 	if (huge_pmd)
