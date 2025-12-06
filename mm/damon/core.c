da8929bc9afe526785960976df3a6250cf544c2e
SeongJae Park <sj@kernel.org>
2025-12-05 17:14:26 -0800

    mm/damon/core: set damon_access_report->vaddr from page fault report
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- mm/damon/core.c
+++ mm/damon/core.c
@@ -1761,6 +1761,7 @@ void damon_report_access(struct damon_access_report *report)
 void damon_report_page_fault(struct vm_fault *vmf, bool huge_pmd)
 {
 	struct damon_access_report access_report = {
+		.vaddr = vmf->address,
 		.size = 1,	/* todo: set appripriately */
 		.cpu = smp_processor_id(),
 		.tid = task_pid_vnr(current),
@@ -1771,7 +1772,6 @@ void damon_report_page_fault(struct vm_fault *vmf, bool huge_pmd)
 		access_report.paddr = PFN_PHYS(pmd_pfn(vmf->orig_pmd));
 	else
 		access_report.paddr = PFN_PHYS(pte_pfn(vmf->orig_pte));
-	/* todo: report vmf->address as virtual address */
 
 	damon_report_access(&access_report);
 }
