7f35bb2188630bae789ddf29ced341790efc5088
Byungchul Park <byungchul@sk.com>
2026-02-24 14:13:47 +0900

    mm: introduce a new page type for page pool in page type
    Currently, the condition 'page->pp_magic == PP_SIGNATURE' is used to
    determine if a page belongs to a page pool.  However, with the planned
    removal of @pp_magic, we should instead leverage the page_type in struct
    page, such as PGTY_netpp, for this purpose.
    
    Introduce and use the page type APIs e.g.  PageNetpp(), __SetPageNetpp(),
    and __ClearPageNetpp() instead, and remove the existing APIs accessing
    @pp_magic e.g.  page_pool_page_is_pp(), netmem_or_pp_magic(), and
    netmem_clear_pp_magic().
    
    Plus, add @page_type to struct net_iov at the same offset as struct page
    so as to use the page_type APIs for struct net_iov as well.  While at it,
    reorder @type and @owner in struct net_iov to avoid a hole and increasing
    the struct size.
    
    This work was inspired by the following link:
    
      https://lore.kernel.org/all/582f41c0-2742-4400-9c81-0d46bf4e8314@gmail.com/
    
    While at it, move the sanity check for page pool to on the free path.
    
    Link: https://lkml.kernel.org/r/20260224051347.19621-1-byungchul@sk.com
    Co-developed-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Byungchul Park <byungchul@sk.com>
    Suggested-by: David Hildenbrand <david@redhat.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Acked-by: Zi Yan <ziy@nvidia.com>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Toke Høiland-Jørgensen <toke@redhat.com>
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Andrew Lunn <andrew+netdev@lunn.ch>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Brendan Jackman <jackmanb@google.com>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Daniel Borkman <daniel@iogearbox.net>
    Cc: Daniel Borkmann <daniel@iogearbox.net>
    Cc: David S. Miller <davem@davemloft.net>
    Cc: David Wei <dw@davidwei.uk>
    Cc: Dragos Tatulea <dtatulea@nvidia.com>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: Ilias Apalodimas <ilias.apalodimas@linaro.org>
    Cc: Jakub Kacinski <kuba@kernel.org>
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: Jesper Dangaard Brouer <hawk@kernel.org>
    Cc: John Fastabend <john.fastabend@gmail.com>
    Cc: Leon Romanovsky <leon@kernel.org>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Mark Bloch <mbloch@nvidia.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mina Almasry <almasrymina@google.com>
    Cc: Paolo Abeni <pabeni@redhat.com>
    Cc: Saeed Mahameed <saeedm@nvidia.com>
    Cc: Simon Horman <horms@kernel.org>
    Cc: Stanislav Fomichev <sdf@fomichev.me>
    Cc: Stehen Rothwell <sfr@canb.auug.org.au>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Taehee Yoo <ap420073@gmail.com>
    Cc: Tariq Toukan <tariqt@nvidia.com>
    Cc: Usama Arif <usamaarif642@gmail.com>
    Cc: Yu Zhao <yuzhao@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1079,7 +1079,6 @@ static inline bool page_expected_state(struct page *page,
 #ifdef CONFIG_MEMCG
 			page->memcg_data |
 #endif
-			page_pool_page_is_pp(page) |
 			(page->flags.f & check_flags)))
 		return false;
 
@@ -1106,8 +1105,6 @@ static const char *page_bad_reason(struct page *page, unsigned long flags)
 	if (unlikely(page->memcg_data))
 		bad_reason = "page still charged to cgroup";
 #endif
-	if (unlikely(page_pool_page_is_pp(page)))
-		bad_reason = "page_pool leak";
 	return bad_reason;
 }
 
@@ -1416,9 +1413,15 @@ __always_inline bool __free_pages_prepare(struct page *page,
 		mod_mthp_stat(order, MTHP_STAT_NR_ANON, -1);
 		folio->mapping = NULL;
 	}
-	if (unlikely(page_has_type(page)))
+	if (unlikely(page_has_type(page))) {
+		/* networking expects to clear its page type before releasing */
+		if (unlikely(PageNetpp(page))) {
+			bad_page(page, "page_pool leak");
+			return false;
+		}
 		/* Reset the page_type (which overlays _mapcount) */
 		page->page_type = UINT_MAX;
+	}
 
 	if (is_check_pages_enabled()) {
 		if (free_page_is_bad(page))
