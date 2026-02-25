1bf152efe508911be083a434b9c9c205eb6ed4cd
Michal Clapinski <mclapinski@google.com>
2026-02-25 16:39:54 +0100

    kho: fix deferred init of kho scratch
    Patch series "kho: add support for deferred struct page init", v5.
    
    When CONFIG_DEFERRED_STRUCT_PAGE_INIT (hereinafter DEFERRED) is enabled,
    struct page initialization is deferred to parallel kthreads that run later
    in the boot process.
    
    Currently, KHO is incompatible with DEFERRED.  This series fixes that
    incompatibility.
    
    
    This patch (of 2):
    
    Currently, mm_core_init calls kho_memory_init, which calls
    kho_release_scratch.
    
    If DEFERRED is enabled, kho_release_scratch will first initialize the
    struct pages of kho scratch.  This is not needed.  We can just let
    page_alloc_init_late init it.
    
    Next, kho_release_scratch will mark scratch as MIGRATE_CMA.  If DEFERRED
    is enabled, this will be overwritten later in deferred_free_pages.
    
    To fix this, I removed the whole kho_release_scratch.  Marking the
    pageblocks as MIGRATE_CMA now happens in kho_init, which runs after
    deferred_free_pages.
    
    Link: https://lkml.kernel.org/r/20260225153955.1006649-1-mclapinski@google.com
    Link: https://lkml.kernel.org/r/20260225153955.1006649-2-mclapinski@google.com
    Signed-off-by: Michal Clapinski <mclapinski@google.com>
    Reviewed-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Cc: Alexander Graf <graf@amazon.com>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Pratyush Yadav <pratyush@kernel.org>
    Cc: Evangelos Petrongonas <epetron@amazon.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/liveupdate/kexec_handover.c
+++ kernel/liveupdate/kexec_handover.c
@@ -1388,11 +1388,6 @@ static __init int kho_init(void)
 	if (err)
 		goto err_free_fdt;
 
-	if (fdt) {
-		kho_in_debugfs_init(&kho_in.dbg, fdt);
-		return 0;
-	}
-
 	for (int i = 0; i < kho_scratch_cnt; i++) {
 		unsigned long base_pfn = PHYS_PFN(kho_scratch[i].addr);
 		unsigned long count = kho_scratch[i].size >> PAGE_SHIFT;
@@ -1408,8 +1403,17 @@ static __init int kho_init(void)
 		 */
 		kmemleak_ignore_phys(kho_scratch[i].addr);
 		for (pfn = base_pfn; pfn < base_pfn + count;
-		     pfn += pageblock_nr_pages)
-			init_cma_reserved_pageblock(pfn_to_page(pfn));
+		     pfn += pageblock_nr_pages) {
+			if (fdt)
+				init_cma_pageblock(pfn_to_page(pfn));
+			else
+				init_cma_reserved_pageblock(pfn_to_page(pfn));
+		}
+	}
+
+	if (fdt) {
+		kho_in_debugfs_init(&kho_in.dbg, fdt);
+		return 0;
 	}
 
 	WARN_ON_ONCE(kho_debugfs_fdt_add(&kho_out.dbg, "fdt",
@@ -1435,35 +1439,10 @@ err_free_scratch:
 }
 fs_initcall(kho_init);
 
-static void __init kho_release_scratch(void)
-{
-	phys_addr_t start, end;
-	u64 i;
-
-	memmap_init_kho_scratch_pages();
-
-	/*
-	 * Mark scratch mem as CMA before we return it. That way we
-	 * ensure that no kernel allocations happen on it. That means
-	 * we can reuse it as scratch memory again later.
-	 */
-	__for_each_mem_range(i, &memblock.memory, NULL, NUMA_NO_NODE,
-			     MEMBLOCK_KHO_SCRATCH, &start, &end, NULL) {
-		ulong start_pfn = pageblock_start_pfn(PFN_DOWN(start));
-		ulong end_pfn = pageblock_align(PFN_UP(end));
-		ulong pfn;
-
-		for (pfn = start_pfn; pfn < end_pfn; pfn += pageblock_nr_pages)
-			init_pageblock_migratetype(pfn_to_page(pfn),
-						   MIGRATE_CMA, false);
-	}
-}
-
 void __init kho_memory_init(void)
 {
 	if (kho_in.scratch_phys) {
 		kho_scratch = phys_to_virt(kho_in.scratch_phys);
-		kho_release_scratch();
 
 		if (kho_mem_retrieve(kho_get_fdt()))
 			kho_in.fdt_phys = 0;
