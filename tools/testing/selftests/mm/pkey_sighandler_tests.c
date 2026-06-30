3117f238ebe460875cfee0371cf861bcd56b4402
Hongfu Li <lihongfu@kylinos.cn>
2026-06-30 15:32:33 +0800

    selftests/mm: use pkey_assert on clone_raw failure in pkey test
    Use pkey_assert(0) instead of perror("clone") when clone_raw() fails.  The
    old path only printed an error and continued; the test now exits via
    pkey_assert() on failure so it does not hang or proceed with an invalid
    child.
    
    Link: https://lore.kernel.org/20260630073235.3802271-4-lihongfu@kylinos.cn
    Signed-off-by: Hongfu Li <lihongfu@kylinos.cn>
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Acked-by: Liam R. Howlett (Oracle) <liam@infradead.org>
    Tested-by: Kevin Brodsky <kevin.brodsky@arm.com>
    Reviewed-by: Kevin Brodsky <kevin.brodsky@arm.com>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Joey Gouly <joey.gouly@arm.com>
    Cc: John Hubbard <jhubbard@nvidia.com>
    Cc: Keith Lucas <keith.lucas@oracle.com>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Madhavan Srinivasan <maddy@linux.ibm.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Muhammad Usama Anjum <usama.anjum@collabora.com>
    Cc: Ross Zwisler <zwisler@google.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: Yury Khrustalev <yury.khrustalev@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- tools/testing/selftests/mm/pkey_sighandler_tests.c
+++ tools/testing/selftests/mm/pkey_sighandler_tests.c
@@ -333,7 +333,7 @@ static void test_sigsegv_handler_with_different_pkey_for_stack(void)
 
 	if (ret < 0) {
 		errno = -ret;
-		perror("clone");
+		pkey_assert(0);
 	} else if (ret == 0) {
 		thread_segv_maperr_ptr(&sigstack);
 		syscall_raw(SYS_exit, 0, 0, 0, 0, 0, 0);
@@ -506,7 +506,7 @@ static void test_pkru_sigreturn(void)
 
 	if (ret < 0) {
 		errno = -ret;
-		perror("clone");
+		pkey_assert(0);
 	}  else if (ret == 0) {
 		thread_sigusr2_self(&sigstack);
 		syscall_raw(SYS_exit, 0, 0, 0, 0, 0, 0);
