9075c1a567405e6b44a4f1df0b3ee0cdc22ae446
SJ Park <sj@kernel.org>
2026-08-15 09:41:50 -0700

    mm/damon/vaddr: use damon_ops_filter_match()
    Signed-off-by: SJ Park <sj@kernel.org>

--- mm/damon/vaddr.c
+++ mm/damon/vaddr.c
@@ -541,29 +541,8 @@ static bool damon_va_filter_match(struct damon_filter *filter,
 		struct mm_struct *mm, unsigned long addr)
 {
 	bool matched = false;
-	struct mem_cgroup *memcg;
 
 	switch (filter->type) {
-	case DAMON_FILTER_TYPE_ANON:
-		if (!folio) {
-			matched = false;
-			break;
-		}
-		matched = folio_test_anon(folio);
-		break;
-	case DAMON_FILTER_TYPE_MEMCG:
-		if (!folio) {
-			matched = false;
-			break;
-		}
-		rcu_read_lock();
-		memcg = folio_memcg_check(folio);
-		if (!memcg)
-			matched = false;
-		else
-			matched = filter->memcg_id == mem_cgroup_id(memcg);
-		rcu_read_unlock();
-		break;
 	case DAMON_FILTER_TYPE_PGIDLE_UNSET:
 		if (!folio)
 			matched = false;
@@ -572,7 +551,7 @@ static bool damon_va_filter_match(struct damon_filter *filter,
 					addr);
 		break;
 	default:
-		break;
+		return damon_ops_filter_match(filter, folio);
 	}
 	return matched == filter->matching;
 }
