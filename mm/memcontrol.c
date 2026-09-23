d531e15246939c4cb38e533b7459d16e96903fc4
Andrew Morton <akpm@linux-foundation.org>
2026-09-22 20:33:18 -0700

    mm-memcontrol-drop-kmemcg_id-and-use-mem_cgroup_id-for-list_lru-indexing-fix
    remove kmemcg_id from memcg_struct_check()
    
    Cc: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -5887,8 +5887,6 @@ static void __init memcg_struct_check(void)
 	CACHELINE_ASSERT_GROUP_MEMBER(struct mem_cgroup, memcg_read_mostly,
 				      private_id);
 	CACHELINE_ASSERT_GROUP_MEMBER(struct mem_cgroup, memcg_read_mostly,
-				      kmemcg_id);
-	CACHELINE_ASSERT_GROUP_MEMBER(struct mem_cgroup, memcg_read_mostly,
 				      oom_group);
 
 	CACHELINE_ASSERT_GROUP_MEMBER(struct mem_cgroup_per_node,
