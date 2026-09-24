854f7d363fcbbaab9d8a88a9a67ac66f3e7afa11
Yeoreum Yun <yeoreum.yun@arm.com>
2026-09-24 20:11:16 +0100

    kselftest: mm: prevent random failure of huge page split for khugepaged
    Patch series "kselftest: mm: fix some failure of split_huge_page_test", v8.
    
    split_huge_page_test can fail for the following reasons:
    
      1. During the test, khugepaged may collapse previously split pages again,
         causing intermittent failures.
    
      2. Since glibc commit 321e1fc73f (“malloc: Enable 2MB THP by default on AArch64”),
         glibc may call madvise(MADV_HUGEPAGE) for sufficiently large allocations
         made by memalign(). The underlying VMA may start at a different address
         from the aligned address returned by memalign(). Moreover, a subsequent
         madvise(MADV_HUGEPAGE) call does not split the VMA because it already
         has the same advice.
    
         This causes the test to fail because the check_huge_xxx() helpers
         incorrectly require the address returned by memalign() to match the
         VMA start address reported in /proc/self/smaps.
    
    Address these issues by applying MADV_NOHUGEPAGE after faulting in the
    huge page, preventing khugepaged from collapsing it again, and instead of
    relying on /proc/self/smaps, use /proc/self/pagemap and /proc/kpageflags
    to detect huge-page mappings and large folios:
    
      1. If hpage_size == pmd_pagesize, check PAGE_IS_HUGE instead of
         using check_large_folios(), since only the mapping type matters.
         This identifies PMD-mapped huge pages.
      2. Otherwise, use check_large_folios() to detect large folios. This
         covers mTHP cases.
      3. Check the folio flags according to the type of huge page.
    
    Since check_huge_shmem() was required to distinguish shmem huge pages
    because /proc/self/smaps reports them using a dedicated
    “ShmemPmdMapped” entry, as opposed to “FilePmdMapped” for
    file-backed huge pages.
    
    Now that /proc/self/smaps is no longer used to detect huge pages and
    /proc/kpageflags is used instead, it is sufficient to distinguish between
    file-backed and anonymous pages since the ShmemPmdMapped is also kind of
    file-backed.
    
    Therefore, remove check_huge_shmem() and use check_huge_file() instead.
    
    
    This patch (of 4):
    
    There're some random failure for split_huge_page_test when khugepaged
    collapses pages into pmd again which had split by the test.
    
    Prevent the khugepaged's collapses for split page by setting the mapped
    pmd-huge-page with MADV_NOHUGEPAGE before split.
    
    Link: https://lore.kernel.org/20260924-fix_split-v8-0-cba7359d882a@arm.com
    Link: https://lore.kernel.org/20260924-fix_split-v8-1-cba7359d882a@arm.com
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Suggested-by: Kevin Brodsky <kevin.brodsky@arm.com>
    Suggested-by: Lorenzo Stoakes (ARM) <ljs@kernel.org>
    Reviewed-by: Lorenzo Stoakes (ARM) <ljs@kernel.org>
    Reviewed-by: Zi Yan <ziy@nvidia.com>
    Acked-by: David Hildenbrand (Arm) <david@kernel.org>
    Reviewed-by: Sarthak Sharma <sarthak.sharma@arm.com>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Nico Pache <nico.pache@linux.dev>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Dev Jain <dev.jain@arm.com>
    Cc: Barry Song <baohua@kernel.org>
    Cc: Lance Yang <lance.yang@linux.dev>
    Cc: Usama Arif <usama.arif@linux.dev>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Shuah Khan <shuah@kernel.org>

--- tools/testing/selftests/mm/split_huge_page_test.c
+++ tools/testing/selftests/mm/split_huge_page_test.c
@@ -108,6 +108,18 @@ static char *allocate_zero_filled_hugepage(size_t len)
 	return result;
 }
 
+static void disable_khugepaged(void *addr, size_t len)
+{
+	/*
+	 * Disables khugepaged from collapsing THPs in range, existing THP
+	 * pages remain.
+	 */
+	if (!madvise(addr, len, MADV_NOHUGEPAGE))
+		return;
+
+	ksft_exit_fail_msg("MADV_NOHUGEPAGE failed, err=%d\n", errno);
+}
+
 static void verify_rss_anon_split_huge_page_all_zeroes(char *one_page, int nr_hpages, size_t len)
 {
 	unsigned long rss_anon_before, rss_anon_after;
@@ -119,6 +131,8 @@ static void verify_rss_anon_split_huge_page_all_zeroes(char *one_page, int nr_hp
 	rss_anon_before = rss_anon();
 	if (!rss_anon_before)
 		ksft_exit_fail_msg("No RssAnon is allocated before split\n");
+
+	disable_khugepaged(one_page, len);
 
 	/* split all THPs */
 	write_debugfs(PID_FMT, getpid(), (uint64_t)one_page,
@@ -167,6 +181,8 @@ static void split_pmd_thp_to_order(int order)
 	if (!check_huge_anon(one_page, 4 * pmd_pagesize, 4, pmd_pagesize))
 		ksft_exit_fail_msg("No THP is allocated\n");
 
+	disable_khugepaged(one_page, len);
+
 	/* split all THPs */
 	write_debugfs(PID_FMT, getpid(), (uint64_t)one_page,
 		(uint64_t)one_page + len, order);
@@ -214,6 +230,8 @@ static void split_pte_mapped_thp(void)
 		ksft_test_result_skip("Not all THPs allocated\n");
 		goto out;
 	}
+
+	disable_khugepaged(thp_area, thp_area_size);
 
 	/*
 	 * To challenge spitting code, we will mremap a single page of each
@@ -482,6 +500,7 @@ static int create_pagecache_thp_and_fd(const char *testfile, size_t fd_size,
 		ksft_test_result_skip("Pagecache folio split skipped\n");
 		return -2;
 	}
+	disable_khugepaged(*addr, fd_size);
 	return 0;
 err_out_close:
 	close(*fd);
